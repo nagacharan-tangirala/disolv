f4439c85fb7bd2bd2c048b52c0e8b160bb00da18
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-29 23:27:56 +0200

    added: second version of constants

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,1 +1,41 @@
 pub(crate) const STREAM_TIME: usize = 1800;
+
+pub(crate) const VEHICLE_TRACE: &str = "vehicle_traces";
+
+pub(crate) const V2V_LINKS: &str = "v2v_links";
+pub(crate) const V2B_LINKS: &str = "v2b_links";
+pub(crate) const V2R_LINKS: &str = "v2r_links";
+pub(crate) const B2C_LINKS: &str = "b2c_links";
+pub(crate) const R2B_LINKS: &str = "r2b_links";
+pub(crate) const R2R_LINKS: &str = "r2r_links";
+
+pub(crate) const VEHICLE_ACTIVATIONS: &str = "vehicle_activations";
+pub(crate) const CONTROLLER_ACTIVATIONS: &str = "controller_activations";
+pub(crate) const BASE_STATION_ACTIVATIONS: &str = "base_station_activations";
+pub(crate) const RSU_ACTIVATIONS: &str = "rsu_activations";
+
+pub(crate) const DATA_SOURCE_CONFIG: &str = "data_source_config";
+
+pub(crate) const VEHICLES: &str = "vehicles";
+pub(crate) const BASE_STATIONS: &str = "base_stations";
+pub(crate) const CONTROLLERS: &str = "controllers";
+pub(crate) const ROADSIDE_UNITS: &str = "roadside_units";
+pub(crate) const EDGE_ORCHESTRATOR: &str = "edge_orchestrator";
+pub(crate) const CLOUD_ORCHESTRATOR: &str = "cloud_orchestrator";
+
+pub(crate) const VEHICLE_ID: &str = "vehicle_id";
+pub(crate) const BASE_STATION_ID: &str = "base_station_id";
+pub(crate) const CONTROLLER_ID: &str = "controller_id";
+pub(crate) const RSU_ID: &str = "rsu_id";
+pub(crate) const LINK_ID: &str = "link_id";
+pub(crate) const DEVICE_ID: &str = "device_id";
+
+pub(crate) const START_TIME: &str = "start_time";
+pub(crate) const END_TIME: &str = "end_time";
+pub(crate) const TIME_STEP: &str = "time_step";
+
+pub(crate) const VELOCITY: &str = "velocity";
+pub(crate) const VEHICLES_STR: &str = "vehicles_str";
+pub(crate) const BASE_STATIONS_STR: &str = "base_stations_str";
+pub(crate) const ROADSIDE_UNITS_STR: &str = "roadside_units_str";
+pub(crate) const DISTANCES_STR: &str = "distances_str";
