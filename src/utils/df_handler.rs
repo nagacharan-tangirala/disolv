276490010f8f0d3646122027a2c21218de46d0e7
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-30 22:57:15 +0200

    modified: separate methods for series to vec conversion, changed trace return to map

--- src/utils/df_handler.rs
+++ src/utils/df_handler.rs
@@ -1,77 +1,118 @@
-use crate::utils::constants::{DEVICE_ID, END_TIME, START_TIME, TIME_STEP, VEHICLE_ID};
+use crate::utils::constants::{
+    COORD_X, COORD_Y, DEVICE_ID, END_TIME, START_TIME, TIME_STEP, VEHICLE_ID, VELOCITY,
+};
 use krabmaga::hashbrown::HashMap;
-use polars::prelude::{col, lit, IntoLazy};
+use polars::prelude::{col, lit, IntoLazy, PolarsResult};
 use polars_core::frame::DataFrame;
 use polars_core::prelude::Series;
 
-pub(crate) struct VehicleTraceDFHandler {
+type Trace = (Vec<i64>, Vec<f32>, Vec<f32>, Vec<f32>);
+type Activation = (Vec<i64>, Vec<i64>);
+
+pub(crate) fn convert_series_to_integer_vector(
+    df: &DataFrame,
+    column_name: &str,
+) -> Result<Vec<i64>, Box<dyn std::error::Error>> {
+    let column_as_series: &Series = match df.columns([column_name])?.get(0) {
+        Some(series) => *series,
+        None => return Err("Error in the column name".into()),
+    };
+    let series_to_list: PolarsResult<Series> = column_as_series.explode();
+    let list_to_option_vec: Vec<Option<i64>> = series_to_list.unwrap().i64()?.to_vec();
+    let option_vec_to_vec: Vec<i64> = list_to_option_vec
+        .iter()
+        .map(|x| x.unwrap_or(-1))
+        .collect::<Vec<i64>>();
+
+    if option_vec_to_vec.contains(&-1) {
+        return Err("Error in converting series to vector".into());
+    }
+    return Ok(option_vec_to_vec);
+}
+
+pub(crate) fn convert_series_to_floating_vector(
+    df: &DataFrame,
+    column_name: &str,
+) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
+    let column_as_series: &Series = match df.columns([column_name])?.get(0) {
+        Some(series) => *series,
+        None => return Err("Error in the column name".into()),
+    };
+    let series_to_list: PolarsResult<Series> = column_as_series.explode();
+    let list_to_option_vec: Vec<Option<f32>> = series_to_list.unwrap().f32()?.to_vec();
+    let option_vec_to_vec: Vec<f32> = list_to_option_vec
+        .iter()
+        .map(|x| x.unwrap_or(-1.))
+        .collect::<Vec<f32>>();
+
+    if option_vec_to_vec.iter().map(|x| x < &0.).any(|x| x) {
+        return Err("Error in converting series to vector".into());
+    }
+    return Ok(option_vec_to_vec);
+}
+
+pub(crate) struct TraceDFHandler {
     trace_df: DataFrame,
 }
 
-impl VehicleTraceDFHandler {
+impl TraceDFHandler {
     pub(crate) fn new(trace_df: DataFrame) -> Self {
         Self { trace_df }
     }
 
-    #[rustfmt::skip]
-    pub(crate) fn prepare_trace_dfs(
+    pub(crate) fn prepare_trace_data(
         &mut self,
-    ) -> Result<HashMap<i64, DataFrame>, Box<dyn std::error::Error>> {
-
-        let filtered_df = self.trace_df
+        device_id_column: &str,
+    ) -> Result<HashMap<i64, Trace>, Box<dyn std::error::Error>> {
+        let filtered_df: DataFrame = self
+            .trace_df
             .clone()
             .lazy()
-            .groupby(
-                [col("vehicle_id")]
-            )
+            .groupby([col(VEHICLE_ID)])
             .agg(
-                vec![
-                    col("time"),
-                    col("x"),
-                    col("y"),
-                ]
+                vec![col(TIME_STEP), col(COORD_X), col(COORD_Y), col(VELOCITY)]
                     .into_iter()
                     .collect::<Vec<_>>(),
             )
-            .collect().unwrap();
+            .collect()
+            .unwrap();
 
-        let vehicle_series: &Series = match filtered_df.columns(["vehicle_id"])?.get(0) {
-            Some(series) => *series,
-            None => return Err("No vehicle_id column found".into()),
-        };
+        let device_ids: Vec<i64> =
+            convert_series_to_integer_vector(&filtered_df, device_id_column)?;
 
-        let vehicle_ids = vehicle_series.i64()?.to_vec().iter()
-            .map(|x| x.unwrap_or(0) ).collect::<Vec<i64>>();
-        if vehicle_ids.contains(&0) {
-            return Err("Vehicle ids should not be 0".into());
-        }
-
-        let mut vehicle_dfs: HashMap<i64, DataFrame> = HashMap::new();
-        for vehicle_id in vehicle_ids.iter() {
+        let mut device_traces: HashMap<i64, Trace> = HashMap::new();
+        for device_id in device_ids.iter() {
             let vehicle_df = filtered_df
                 .clone()
                 .lazy()
-                .filter(col("vehicle_id").eq(lit(*vehicle_id)))
+                .filter(col(device_id_column).eq(lit(*device_id)))
                 .collect()
                 .unwrap();
-            vehicle_dfs.insert(*vehicle_id, vehicle_df);
+
+            let time_steps: Vec<i64> = convert_series_to_integer_vector(&vehicle_df, TIME_STEP)?;
+            let x_positions: Vec<f32> = convert_series_to_floating_vector(&vehicle_df, COORD_X)?;
+            let y_positions: Vec<f32> = convert_series_to_floating_vector(&vehicle_df, COORD_Y)?;
+            let velocities: Vec<f32> = convert_series_to_floating_vector(&vehicle_df, VELOCITY)?;
+
+            let trace: Trace = (time_steps, x_positions, y_positions, velocities);
+            device_traces.insert(*device_id, trace);
         }
-        return Ok(vehicle_dfs);
+        return Ok(device_traces);
     }
 }
 
-pub struct ActivationHandler {
+pub(crate) struct ActivationDFHandler {
     activation_df: DataFrame,
 }
 
-impl ActivationHandler {
-    pub fn new(activation_df: DataFrame) -> Self {
+impl ActivationDFHandler {
+    pub(crate) fn new(activation_df: DataFrame) -> Self {
         Self { activation_df }
     }
 
-    pub fn prepare_device_activations(
+    pub(crate) fn prepare_device_activations(
         &mut self,
-    ) -> Result<HashMap<i64, (Vec<i64>, Vec<i64>)>, Box<dyn std::error::Error>> {
+    ) -> Result<HashMap<i64, Activation>, Box<dyn std::error::Error>> {
         let filtered_df = self
             .activation_df
             .clone()
