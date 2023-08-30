7edaa4b9eaac8b1d97aa68654571ffa5622a6fbc
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-30 22:53:46 +0200

    removed: unused constants

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,27 +1,9 @@
 pub(crate) const STREAM_TIME: usize = 1800;
 
-pub(crate) const VEHICLE_TRACE: &str = "vehicle_traces";
-
-pub(crate) const V2V_LINKS: &str = "v2v_links";
-pub(crate) const V2B_LINKS: &str = "v2b_links";
-pub(crate) const V2R_LINKS: &str = "v2r_links";
-pub(crate) const B2C_LINKS: &str = "b2c_links";
-pub(crate) const R2B_LINKS: &str = "r2b_links";
-pub(crate) const R2R_LINKS: &str = "r2r_links";
-
-pub(crate) const VEHICLE_ACTIVATIONS: &str = "vehicle_activations";
-pub(crate) const CONTROLLER_ACTIVATIONS: &str = "controller_activations";
-pub(crate) const BASE_STATION_ACTIVATIONS: &str = "base_station_activations";
-pub(crate) const RSU_ACTIVATIONS: &str = "rsu_activations";
-
-pub(crate) const DATA_SOURCE_CONFIG: &str = "data_source_config";
-
 pub(crate) const VEHICLES: &str = "vehicles";
 pub(crate) const BASE_STATIONS: &str = "base_stations";
 pub(crate) const CONTROLLERS: &str = "controllers";
 pub(crate) const ROADSIDE_UNITS: &str = "roadside_units";
-pub(crate) const EDGE_ORCHESTRATOR: &str = "edge_orchestrator";
-pub(crate) const CLOUD_ORCHESTRATOR: &str = "cloud_orchestrator";
 
 pub(crate) const VEHICLE_ID: &str = "vehicle_id";
 pub(crate) const BASE_STATION_ID: &str = "base_station_id";
