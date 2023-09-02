0f9c6751443e990c996d982623c46b7ace6a350e
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-02 16:47:40 +0200

    added: defaults for enums, fixed parsing related bugs

--- src/utils/ds_config.rs
+++ src/utils/ds_config.rs
@@ -2,35 +2,38 @@ use serde_derive::Deserialize;
 use std::collections::HashMap as Map;
 use std::path::PathBuf;
 
-#[derive(Deserialize, Debug, Copy, Clone)]
+#[derive(Deserialize, Default, Debug, Copy, Clone)]
 pub(crate) enum DataTargetType {
+    #[default]
+    None = 0,
     Vehicle,
     RSU,
     BaseStation,
 }
 
-#[derive(Deserialize, Debug, Copy, Clone)]
-pub(crate) enum DataType {
+#[derive(Deserialize, Default, Debug, Hash, Copy, Clone, PartialEq, Eq)]
+pub(crate) enum SensorType {
+    #[default]
+    None = 0,
     Image,
     Video,
-    Lidar3D,
     Lidar2D,
+    Lidar3D,
     Radar,
     Other,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct AllDataSources {
-    pub(crate) vehicle_sources: Map<String, DataSourceSettings>,
-    pub(crate) rsu_sources: Map<String, DataSourceSettings>,
+    pub(crate) vehicle_sources: Map<String, Map<String, DataSourceSettings>>,
+    pub(crate) rsu_sources: Map<String, Map<String, DataSourceSettings>>,
 }
 
-#[derive(Deserialize, Debug, Clone, Copy)]
+#[derive(Deserialize, Default, Debug, Clone, Copy)]
 pub(crate) struct DataSourceSettings {
-    pub(crate) id: u8,
-    pub(crate) data_type: DataType,
+    pub(crate) data_type: SensorType,
     pub(crate) data_counts: u16,
-    pub(crate) data_size: f32,
+    pub(crate) unit_size: f32,
     pub(crate) target_type: DataTargetType,
 }
 
@@ -41,9 +44,7 @@ pub(crate) struct DSConfigReader {
 impl DSConfigReader {
     pub(crate) fn new(file_name: &PathBuf) -> Self {
         let file_path = PathBuf::from(file_name);
-        Self {
-            file_path: file_path,
-        }
+        Self { file_path }
     }
 
     pub(crate) fn parse(&self) -> Result<AllDataSources, Box<dyn std::error::Error>> {
