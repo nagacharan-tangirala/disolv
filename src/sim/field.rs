b1ae4e2952579a16a29ba36bd73f4d9049404da4
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-07 13:36:13 +0200

    added: methods to refresh position cache for the devices

--- src/sim/field.rs
+++ src/sim/field.rs
@@ -105,40 +105,17 @@ impl DeviceField {
         self.controller_positions = self.read_controller_positions();
     }
 
-    pub(crate) fn before_step(&mut self, step: u64) {
-        self.step = step;
+    pub(crate) fn clear(&mut self) {
         self.position_cache = HashMap::new();
         self.velocity_cache = HashMap::new();
-        if let Some(vehicle_traces) = self.vehicle_positions.get(&self.step) {
-            if let vehicle_traces = vehicle_traces.as_ref().unwrap() {
-                let (vehicle_ids, xs, ys, velocities) = vehicle_traces;
-                for (vehicle_id, x, y, velocity) in
-                    izip!(vehicle_ids.iter(), xs.iter(), ys.iter(), velocities.iter())
-                {
-                    self.position_cache
-                        .insert(*vehicle_id, Real2D { x: *x, y: *y });
-                    self.velocity_cache.insert(*vehicle_id, *velocity);
-                }
-            }
-        }
-        if let Some(rsu_positions) = self.rsu_positions.get(&self.step) {
-            if let rsu_positions = rsu_positions.as_ref().unwrap() {
-                let (rsu_ids, xs, ys, _) = rsu_positions;
-                for (rsu_id, x, y) in izip!(rsu_ids.iter(), xs.iter(), ys.iter()) {
-                    self.position_cache.insert(*rsu_id, Real2D { x: *x, y: *y });
-                    self.velocity_cache.insert(*rsu_id, 0.0);
-                }
-            }
-        }
-        if let Some(bs_positions) = self.bs_positions.get(&self.step) {
-            if let bs_positions = bs_positions.as_ref().unwrap() {
-                let (bs_ids, xs, ys, _) = bs_positions;
-                for (bs_id, x, y) in izip!(bs_ids.iter(), xs.iter(), ys.iter()) {
-                    self.position_cache.insert(*bs_id, Real2D { x: *x, y: *y });
-                    self.velocity_cache.insert(*bs_id, 0.0);
-                }
-            }
-        }
+    }
+
+    pub(crate) fn before_step(&mut self, step: TimeStamp) {
+        self.step = step;
+        self.clear();
+        self.refresh_vehicle_cache();
+        self.refresh_rsu_cache();
+        self.refresh_bs_cache();
     }
 
     pub(crate) fn update(&mut self) {
@@ -146,6 +123,61 @@ impl DeviceField {
         self.rsu_field.lazy_update();
         self.bs_field.lazy_update();
         self.controller_field.lazy_update();
+    }
+
+    fn refresh_vehicle_cache(&mut self) {
+        if let Some(vehicle_traces) = self.vehicle_positions.get(&self.step) {
+            let (vehicle_ids, xs, ys, velocities) = match vehicle_traces.as_ref() {
+                Some(vehicle_traces) => vehicle_traces,
+                None => {
+                    debug! {"No vehicle traces found at step {}", self.step}
+                    return;
+                }
+            };
+            for (vehicle_id, x, y, velocity) in
+                izip!(vehicle_ids.iter(), xs.iter(), ys.iter(), velocities.iter())
+            {
+                self.position_cache
+                    .insert(*vehicle_id, Real2D { x: *x, y: *y });
+                self.velocity_cache.insert(*vehicle_id, *velocity);
+            }
+        }
+    }
+
+    fn refresh_rsu_cache(&mut self) {
+        if let Some(rsu_traces) = self.rsu_positions.get(&self.step) {
+            let (rsu_ids, xs, ys, velocities) = match rsu_traces.as_ref() {
+                Some(vehicle_traces) => vehicle_traces,
+                None => {
+                    debug! {"No RSU traces found at step {}", self.step}
+                    return;
+                }
+            };
+            for (rsu_id, x, y, _velocity) in
+                izip!(rsu_ids.iter(), xs.iter(), ys.iter(), velocities.iter())
+            {
+                self.position_cache.insert(*rsu_id, Real2D { x: *x, y: *y });
+                self.velocity_cache.insert(*rsu_id, 0.0);
+            }
+        }
+    }
+
+    fn refresh_bs_cache(&mut self) {
+        if let Some(bs_positions) = self.bs_positions.get(&self.step) {
+            let (bs_ids, xs, ys, velocities) = match bs_positions.as_ref() {
+                Some(vehicle_traces) => vehicle_traces,
+                None => {
+                    debug! {"No base station traces found at step {}", self.step}
+                    return;
+                }
+            };
+            for (bs_id, x, y, velocity) in
+                izip!(bs_ids.iter(), xs.iter(), ys.iter(), velocities.iter())
+            {
+                self.position_cache.insert(*bs_id, Real2D { x: *x, y: *y });
+                self.velocity_cache.insert(*bs_id, 0.0);
+            }
+        }
     }
 
     pub(crate) fn refresh_position_data(&mut self, step: u64) {
