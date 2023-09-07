47b5192fe2d471f4b7bad66378ffa82178a0cbd2
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-07 13:38:01 +0200

    modified: the datatypes to use shorter aliases

--- src/data/data_io.rs
+++ src/data/data_io.rs
@@ -1,15 +1,15 @@
 use crate::data::{df_handler, df_utils, file_io};
+use crate::sim::field::TraceMap;
+use crate::sim::vanet::{Link, MultiLinkMap, SingleLinkMap};
 use crate::utils::constants::{COL_CONTROLLER_ID, COL_COORD_X, COL_COORD_Y};
 use krabmaga::hashbrown::HashMap;
 use log::warn;
-use polars_io::SerReader;
 use std::path::PathBuf;
 
 pub(crate) type DeviceId = u64;
 pub(crate) type TimeStamp = u64;
 pub(crate) type Activation = (Vec<TimeStamp>, Vec<TimeStamp>); // (start_time, end_time)
 pub(crate) type Trace = (Vec<DeviceId>, Vec<f32>, Vec<f32>, Vec<f32>); // (device_id, x, y, velocity)
-pub(crate) type Link = (Vec<DeviceId>, Vec<f32>); // (neighbour_ids, distances)
 
 pub(crate) fn read_activation_data(activations_file: PathBuf) -> HashMap<DeviceId, Activation> {
     let activation_df = match file_io::read_csv_data(activations_file) {
@@ -29,10 +29,7 @@ pub(crate) fn read_activation_data(activations_file: PathBuf) -> HashMap<DeviceI
     return activations_map;
 }
 
-pub(crate) fn read_all_positions(
-    trace_file: PathBuf,
-    device_id_column: &str,
-) -> HashMap<TimeStamp, Option<Trace>> {
+pub(crate) fn read_all_positions(trace_file: PathBuf, device_id_column: &str) -> TraceMap {
     let trace_df = match file_io::read_parquet_data(trace_file) {
         Ok(trace_df) => trace_df,
         Err(e) => {
@@ -40,13 +37,12 @@ pub(crate) fn read_all_positions(
         }
     };
 
-    let trace_map: HashMap<TimeStamp, Option<Trace>> =
-        match df_handler::prepare_trace_data(&trace_df, device_id_column) {
-            Ok(trace_map) => trace_map,
-            Err(e) => {
-                panic!("Error while converting DF to hashmap: {}", e);
-            }
-        };
+    let trace_map: TraceMap = match df_handler::prepare_trace_data(&trace_df, device_id_column) {
+        Ok(trace_map) => trace_map,
+        Err(e) => {
+            panic!("Error while converting DF to hashmap: {}", e);
+        }
+    };
     return trace_map;
 }
 
@@ -69,7 +65,7 @@ pub(crate) fn read_controller_positions(
     return Ok(controller_map);
 }
 
-pub(crate) fn read_bs2c_links(b2c_links_file: PathBuf) -> HashMap<DeviceId, DeviceId> {
+pub(crate) fn read_bs2c_links(b2c_links_file: PathBuf) -> SingleLinkMap {
     let bs2c_links_df = match file_io::read_csv_data(b2c_links_file) {
         Ok(b2c_links_df) => b2c_links_df,
         Err(e) => {
@@ -77,17 +73,16 @@ pub(crate) fn read_bs2c_links(b2c_links_file: PathBuf) -> HashMap<DeviceId, Devi
         }
     };
 
-    let bs2c_links_map: HashMap<DeviceId, DeviceId> =
-        match df_handler::prepare_b2c_links(&bs2c_links_df) {
-            Ok(b2c_links_map) => b2c_links_map,
-            Err(e) => {
-                panic!("Error while converting BS2C links DF to hashmap: {}", e);
-            }
-        };
+    let bs2c_links_map: SingleLinkMap = match df_handler::prepare_b2c_links(&bs2c_links_df) {
+        Ok(b2c_links_map) => b2c_links_map,
+        Err(e) => {
+            panic!("Error while converting BS2C links DF to hashmap: {}", e);
+        }
+    };
     return bs2c_links_map;
 }
 
-pub(crate) fn read_c2c_links(c2c_links_file: PathBuf) -> HashMap<DeviceId, DeviceId> {
+pub(crate) fn read_c2c_links(c2c_links_file: PathBuf) -> SingleLinkMap {
     let c2c_links_df = match file_io::read_csv_data(c2c_links_file) {
         Ok(c2c_links_df) => c2c_links_df,
         Err(e) => {
@@ -95,14 +90,13 @@ pub(crate) fn read_c2c_links(c2c_links_file: PathBuf) -> HashMap<DeviceId, Devic
         }
     };
 
-    let c2c_links_map: HashMap<DeviceId, DeviceId> =
-        match df_handler::prepare_c2c_links(&c2c_links_df) {
-            Ok(c2c_links_map) => c2c_links_map,
-            Err(e) => {
-                warn!("Optional C2C links file has problems. Continuing.");
-                HashMap::new()
-            }
-        };
+    let c2c_links_map: SingleLinkMap = match df_handler::prepare_c2c_links(&c2c_links_df) {
+        Ok(c2c_links_map) => c2c_links_map,
+        Err(_e) => {
+            warn!("Optional C2C links file has problems. Continuing.");
+            HashMap::new()
+        }
+    };
     return c2c_links_map;
 }
 
@@ -110,9 +104,9 @@ pub(crate) fn read_all_links(
     links_file: PathBuf,
     device_id_column: &str,
     neighbour_column: &str,
-) -> Result<HashMap<TimeStamp, HashMap<DeviceId, Link>>, Box<dyn std::error::Error>> {
+) -> Result<HashMap<TimeStamp, MultiLinkMap>, Box<dyn std::error::Error>> {
     let links_df = file_io::read_csv_data(links_file)?;
-    let static_links: HashMap<u64, HashMap<u64, Link>> =
+    let static_links: HashMap<TimeStamp, MultiLinkMap> =
         df_handler::prepare_static_links(&links_df, device_id_column, neighbour_column)?;
     return Ok(static_links);
 }
