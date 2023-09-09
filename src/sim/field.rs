eb53e54ed30c09a4e3885f26ec9e225d4eee8503
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-09 22:34:14 +0200

    refactored: geo data related reading is moved into its own module

--- src/sim/field.rs
+++ src/sim/field.rs
@@ -1,12 +1,10 @@
-use crate::data::data_io;
-use crate::data::data_io::{DeviceId, TimeStamp, Trace};
-use crate::data::stream_io;
 use crate::device::base_station::BaseStation;
 use crate::device::controller::Controller;
 use crate::device::roadside_unit::RoadsideUnit;
 use crate::device::vehicle::Vehicle;
-use crate::utils::config::{FieldSettings, PositionFiles, TraceFlags};
-use crate::utils::constants::{COL_BASE_STATION_ID, COL_RSU_ID, COL_VEHICLE_ID};
+use crate::reader::activation::{DeviceId, TimeStamp};
+use crate::reader::geo::GeoReader;
+use crate::utils::config::{FieldSettings, GeoDataFiles, TraceFlags};
 use crate::DISCRETIZATION;
 use itertools::izip;
 use krabmaga::engine::fields::field::Field;
@@ -16,24 +14,22 @@ use krabmaga::hashbrown::HashMap;
 use log::{debug, info};
 use std::path::PathBuf;
 
-pub(crate) type TraceMap = HashMap<TimeStamp, Option<Trace>>;
+pub(crate) type GeoData = (Vec<DeviceId>, Vec<f32>, Vec<f32>, Vec<f32>); // (device_id, x, y, velocity)
+pub(crate) type GeoMap = HashMap<TimeStamp, Option<GeoData>>;
 
 pub(crate) struct DeviceField {
     pub(crate) field_settings: FieldSettings,
-    pub(crate) trace_flags: TraceFlags,
     pub(crate) vehicle_field: Field2D<Vehicle>,
     pub(crate) rsu_field: Field2D<RoadsideUnit>,
     pub(crate) bs_field: Field2D<BaseStation>,
     pub(crate) controller_field: Field2D<Controller>,
-    pub(crate) vehicle_positions: TraceMap,
-    pub(crate) rsu_positions: TraceMap,
-    pub(crate) bs_positions: TraceMap,
+    pub(crate) vehicle_geo_data: GeoMap,
+    pub(crate) rsu_geo_data: GeoMap,
+    pub(crate) bs_geo_data: GeoMap,
     pub(crate) controller_positions: HashMap<DeviceId, (f32, f32)>,
-    pub(crate) position_files: PositionFiles,
-    pub(crate) config_path: PathBuf,
+    pub(crate) geo_reader: GeoReader,
     pub(crate) position_cache: HashMap<DeviceId, Real2D>,
     pub(crate) velocity_cache: HashMap<DeviceId, f32>,
-    pub(crate) streaming_interval: TimeStamp,
     pub(crate) step: TimeStamp,
 }
 
@@ -42,7 +38,7 @@ impl DeviceField {
         field_settings: &FieldSettings,
         trace_flags: &TraceFlags,
         config_path: &PathBuf,
-        position_files: &PositionFiles,
+        position_files: &GeoDataFiles,
         streaming_interval: u64,
     ) -> Self {
         let vehicle_positions = HashMap::new();
@@ -75,32 +71,32 @@ impl DeviceField {
             false,
         );
 
+        let geo_reader =
+            GeoReader::new(config_path, position_files, trace_flags, streaming_interval);
+
         Self {
             field_settings: field_settings.clone(),
-            trace_flags: trace_flags.clone(),
             vehicle_field,
             rsu_field,
             bs_field,
             controller_field,
-            vehicle_positions,
-            rsu_positions,
-            bs_positions,
+            vehicle_geo_data: vehicle_positions,
+            rsu_geo_data: rsu_positions,
+            bs_geo_data: bs_positions,
             controller_positions,
-            position_files: position_files.clone(),
-            config_path: config_path.clone(),
             step: 0,
-            streaming_interval,
             position_cache: Default::default(),
             velocity_cache: Default::default(),
+            geo_reader,
         }
     }
 
     pub(crate) fn init(&mut self) {
         info! {"Initializing the device field"}
-        self.vehicle_positions = self.read_vehicle_positions();
-        self.rsu_positions = self.read_rsu_positions();
-        self.bs_positions = self.read_base_station_positions();
-        self.controller_positions = self.read_controller_positions();
+        self.vehicle_geo_data = self.geo_reader.read_vehicle_geo_data();
+        self.rsu_geo_data = self.geo_reader.read_rsu_geo_data();
+        self.bs_geo_data = self.geo_reader.read_bs_geo_data();
+        self.controller_positions = self.geo_reader.read_controller_geo_data();
     }
 
     pub(crate) fn before_step(&mut self, step: TimeStamp) {
@@ -117,8 +113,18 @@ impl DeviceField {
         self.controller_field.lazy_update();
     }
 
+    pub(crate) fn refresh_position_data(&mut self, step: TimeStamp) {
+        info! {"Refreshing position data from files at step {}", step}
+        self.geo_reader.step = step;
+        let vehicle_positions = self.geo_reader.read_vehicle_geo_data();
+
+        if vehicle_positions.len() > 0 {
+            self.vehicle_geo_data = vehicle_positions;
+        }
+    }
+
     fn refresh_vehicle_cache(&mut self) {
-        if let Some(vehicle_traces) = self.vehicle_positions.remove(&self.step) {
+        if let Some(vehicle_traces) = self.vehicle_geo_data.remove(&self.step) {
             let (vehicle_ids, xs, ys, velocities) = match vehicle_traces {
                 Some(vehicle_traces) => vehicle_traces,
                 None => {
@@ -137,7 +143,7 @@ impl DeviceField {
     }
 
     fn refresh_rsu_cache(&mut self) {
-        if let Some(rsu_traces) = self.rsu_positions.remove(&self.step) {
+        if let Some(rsu_traces) = self.rsu_geo_data.remove(&self.step) {
             let (rsu_ids, xs, ys, velocities) = match rsu_traces {
                 Some(rsu_traces) => rsu_traces,
                 None => {
@@ -155,7 +161,7 @@ impl DeviceField {
     }
 
     fn refresh_bs_cache(&mut self) {
-        if let Some(bs_positions) = self.bs_positions.remove(&self.step) {
+        if let Some(bs_positions) = self.bs_geo_data.remove(&self.step) {
             let (bs_ids, xs, ys, velocities) = match bs_positions {
                 Some(bs_traces) => bs_traces,
                 None => {
@@ -170,89 +176,5 @@ impl DeviceField {
                 self.velocity_cache.insert(*bs_id, 0.0);
             }
         }
-    }
-
-    pub(crate) fn refresh_position_data(&mut self, step: TimeStamp) {
-        info! {"Refreshing position data from files at step {}", step}
-        self.step = step;
-        let vehicle_positions = self.read_vehicle_positions();
-
-        if vehicle_positions.len() > 0 {
-            self.vehicle_positions = vehicle_positions;
-        }
-    }
-
-    fn stream_device_positions(&self, trace_file: PathBuf, device_id_column: &str) -> TraceMap {
-        let starting_time: TimeStamp = self.step;
-        let ending_time: TimeStamp = self.step + self.streaming_interval;
-        let device_positions: TraceMap = stream_io::stream_positions_in_interval(
-            trace_file,
-            device_id_column,
-            starting_time,
-            ending_time,
-        );
-        return device_positions;
-    }
-
-    fn read_vehicle_positions(&self) -> TraceMap {
-        let trace_file = self
-            .config_path
-            .join(&self.position_files.vehicle_positions);
-        if trace_file.exists() == false {
-            panic!("Vehicle trace file is not found.");
-        }
-
-        let vehicle_positions = if self.trace_flags.vehicle == true {
-            self.stream_device_positions(trace_file, COL_VEHICLE_ID)
-        } else {
-            data_io::read_all_positions(trace_file, COL_VEHICLE_ID)
-        };
-        return vehicle_positions;
-    }
-
-    fn read_rsu_positions(&self) -> TraceMap {
-        let trace_file = self.config_path.join(&self.position_files.rsu_positions);
-        if trace_file.exists() == false {
-            panic!("RSU trace file is not found.");
-        }
-
-        let rsu_positions = if self.trace_flags.roadside_unit == true {
-            self.stream_device_positions(trace_file, COL_RSU_ID)
-        } else {
-            data_io::read_all_positions(trace_file, COL_RSU_ID)
-        };
-        return rsu_positions;
-    }
-
-    fn read_base_station_positions(&self) -> TraceMap {
-        let trace_file = self.config_path.join(&self.position_files.bs_positions);
-        if trace_file.exists() == false {
-            panic!("Base station trace file is not found.");
-        }
-
-        let bs_positions = if self.trace_flags.base_station == true {
-            self.stream_device_positions(trace_file, COL_BASE_STATION_ID)
-        } else {
-            data_io::read_all_positions(trace_file, COL_BASE_STATION_ID)
-        };
-        return bs_positions;
-    }
-
-    fn read_controller_positions(&self) -> HashMap<DeviceId, (f32, f32)> {
-        let trace_file = self
-            .config_path
-            .join(&self.position_files.controller_positions);
-        if trace_file.exists() == false {
-            panic!("Controller trace file is not found.");
-        }
-
-        let controller_positions: HashMap<u64, (f32, f32)> =
-            match data_io::read_controller_positions(trace_file) {
-                Ok(controller_positions) => controller_positions,
-                Err(e) => {
-                    panic!("Error while reading controller positions: {}", e);
-                }
-            };
-        controller_positions
     }
 }
