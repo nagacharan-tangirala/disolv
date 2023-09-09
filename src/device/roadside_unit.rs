bfafa5e54609db0e9ed2f3b1052725375906a93e
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-09 22:41:03 +0200

    added: similar device behavior as vehicles

--- src/device/roadside_unit.rs
+++ src/device/roadside_unit.rs
@@ -1,17 +1,17 @@
 use core::fmt;
 use std::hash::{Hash, Hasher};
 
-use crate::data::data_io::DeviceId;
 use crate::device::device_state::{DeviceState, Timing};
-use crate::models::composer::{BasicComposer, ComposerType, RandomComposer};
+use crate::models::composer::{
+    BasicComposer, ComposerType, DevicePayload, RandomComposer, SensorData,
+};
 use crate::models::simplifier::{BasicSimplifier, RandomSimplifier, SimplifierType};
+use crate::reader::activation::{DeviceId, TimeStamp};
 use krabmaga::engine::agent::Agent;
-use krabmaga::engine::fields::field_2d::{toroidal_transform, Location2D};
+use krabmaga::engine::fields::field_2d::Location2D;
 use krabmaga::engine::location::Real2D;
 use krabmaga::engine::state::State;
 use krabmaga::hashbrown::HashMap;
-use krabmaga::rand;
-use krabmaga::rand::Rng;
 use log::debug;
 
 use crate::sim::core::Core;
@@ -19,39 +19,26 @@ use crate::utils::config::RSUSettings;
 use crate::utils::constants::ARRAY_SIZE;
 use crate::utils::ds_config::{DataSourceSettings, DataTargetType, SensorType};
 
-/// The most basic agent should implement Clone, Copy and Agent to be able to be inserted in a Schedule.
 #[derive(Debug, Clone, Copy)]
 pub struct RoadsideUnit {
     pub(crate) id: DeviceId,
     storage: f32,
     pub(crate) location: Real2D,
     pub(crate) timing: Timing,
-    pub(crate) vehicle_info: SensorInfo,
+    pub(crate) sensor_data: SensorData,
     pub(crate) composer: ComposerType,
     pub(crate) simplifier: SimplifierType,
     pub(crate) status: DeviceState,
-}
-
-#[derive(Clone, Debug, Default)]
-pub(crate) struct RSUPayload {
-    pub(crate) id: u32,
-    pub(crate) vehicle_info: SensorInfo,
-    pub(crate) generated_data_size: HashMap<SensorType, f32>,
-    pub(crate) types_with_counts: HashMap<SensorType, u16>,
-    pub(crate) preferred_targets: HashMap<SensorType, DataTargetType>,
-}
-
-#[derive(Clone, Debug, Copy, Default)]
-pub(crate) struct SensorInfo {
-    pub(crate) location: Real2D,
-    pub(crate) speed: f32,
-    pub(crate) temperature: f32,
-    pub(crate) env_temperature: f32,
+    step: TimeStamp,
 }
 
 impl RoadsideUnit {
-    pub(crate) fn new(id: u64, timing_info: Timing, rsu_settings: &RSUSettings) -> Self {
-        let data_sources: [Option<DataSourceSettings>; ARRAY_SIZE] = [None; ARRAY_SIZE];
+    pub(crate) fn new(
+        id: u64,
+        timing_info: Timing,
+        rsu_settings: &RSUSettings,
+        data_sources: [Option<DataSourceSettings>; ARRAY_SIZE],
+    ) -> Self {
         let composer: ComposerType = match rsu_settings.composer.name.as_str() {
             "random" => ComposerType::Random(RandomComposer {
                 data_sources: data_sources.clone(),
@@ -62,7 +49,7 @@ impl RoadsideUnit {
         };
         let simplifier: SimplifierType = match rsu_settings.composer.name.as_str() {
             "random" => SimplifierType::Random(RandomSimplifier {}),
-            _ => SimplifierType::Basic(BasicSimplifier {}),
+            _ => SimplifierType::Basic(BasicSimplifier::new(rsu_settings.simplifier.clone())),
         };
 
         Self {
@@ -70,25 +57,18 @@ impl RoadsideUnit {
             storage: rsu_settings.storage,
             location: Real2D::default(),
             timing: timing_info,
-            vehicle_info: SensorInfo::default(),
+            sensor_data: SensorData::default(),
             composer,
             simplifier,
             status: DeviceState::Inactive,
+            step: 0,
         }
     }
-}
 
-impl Agent for RoadsideUnit {
-    /// Put the code that should happen for each step, for each agent here.
-    fn step(&mut self, state: &mut dyn State) {
-        let core_state = state.as_any_mut().downcast_mut::<Core>().unwrap();
-        let step = core_state.step;
-        // If we are scheduled, we are active
-        self.status = DeviceState::Active;
-
-        match core_state.device_field.position_cache.get(&self.id) {
+    fn update_geo_data(&mut self, core_state: &mut Core) {
+        match core_state.device_field.position_cache.remove(&self.id) {
             Some(loc) => {
-                self.location = *loc;
+                self.location = loc;
                 core_state
                     .device_field
                     .rsu_field
@@ -96,25 +76,106 @@ impl Agent for RoadsideUnit {
             }
             None => {}
         }
+        self.sensor_data.speed = match core_state.device_field.velocity_cache.remove(&self.id) {
+            Some(speed) => speed,
+            None => 0.0,
+        };
+    }
 
-        // If it is time to deactivate, schedule deactivation
-        if step == self.timing.peek_deactivation_time() {
-            self.status = DeviceState::Inactive;
-            self.timing.increment_timing_index();
-            core_state.devices_to_pop.roadside_units.push(self.id);
+    pub(crate) fn deactivate(&mut self, core_state: &mut Core) {
+        self.status = DeviceState::Inactive;
+        self.timing.increment_timing_index();
+        core_state.devices_to_pop.roadside_units.push(self.id);
 
-            let time_stamp = self.timing.pop_activation_time();
-            if time_stamp > step {
-                core_state
-                    .devices_to_add
-                    .roadside_units
-                    .push((self.id, time_stamp));
-            }
+        let time_stamp = self.timing.pop_activation_time();
+        if time_stamp > self.step {
+            core_state
+                .devices_to_add
+                .roadside_units
+                .push((self.id, time_stamp));
         }
     }
 
-    /// Put the code that decides if an agent should be removed or not
-    /// for example in simulation where agents can die
+    pub(crate) fn transfer_data_to_vehicles(&mut self, core_state: &mut Core) {
+        let mut rsu2v_payload = match self.composer {
+            ComposerType::Basic(ref composer) => composer.compose_payload(DataTargetType::Vehicle),
+            ComposerType::Random(ref composer) => composer.compose_payload(),
+        };
+
+        rsu2v_payload = match self.simplifier {
+            SimplifierType::Basic(ref simplifier) => simplifier.simplify_payload(rsu2v_payload),
+            SimplifierType::Random(ref simplifier) => simplifier.simplify_payload(rsu2v_payload),
+        };
+
+        rsu2v_payload.id = self.id;
+        rsu2v_payload.sensor_data = self.sensor_data;
+
+        core_state
+            .vanet
+            .data_payloads
+            .rsu2v_data
+            .insert(self.id, rsu2v_payload);
+    }
+
+    pub(crate) fn transfer_data_to_bs(&mut self, core_state: &mut Core) {
+        let mut rsu2bs_payload = match self.composer {
+            ComposerType::Basic(ref composer) => {
+                composer.compose_payload(DataTargetType::BaseStation)
+            }
+            ComposerType::Random(ref composer) => composer.compose_payload(),
+        };
+        rsu2bs_payload = match self.simplifier {
+            SimplifierType::Basic(ref simplifier) => simplifier.simplify_payload(rsu2bs_payload),
+            SimplifierType::Random(ref simplifier) => simplifier.simplify_payload(rsu2bs_payload),
+        };
+        rsu2bs_payload.id = self.id;
+        rsu2bs_payload.sensor_data = self.sensor_data;
+
+        core_state
+            .vanet
+            .data_payloads
+            .rsu2bs_data
+            .insert(self.id, rsu2bs_payload);
+    }
+
+    pub(crate) fn transfer_data_to_rsu(&mut self, core_state: &mut Core) {
+        let mut rsu2rsu_payload = match self.composer {
+            ComposerType::Basic(ref composer) => composer.compose_payload(DataTargetType::RSU),
+            ComposerType::Random(ref composer) => composer.compose_payload(),
+        };
+        rsu2rsu_payload = match self.simplifier {
+            SimplifierType::Basic(ref simplifier) => simplifier.simplify_payload(rsu2rsu_payload),
+            SimplifierType::Random(ref simplifier) => simplifier.simplify_payload(rsu2rsu_payload),
+        };
+        rsu2rsu_payload.id = self.id;
+        rsu2rsu_payload.sensor_data = self.sensor_data;
+
+        core_state
+            .vanet
+            .data_payloads
+            .rsu2rsu_data
+            .insert(self.id, rsu2rsu_payload);
+    }
+}
+
+impl Agent for RoadsideUnit {
+    fn step(&mut self, state: &mut dyn State) {
+        debug!("{} is active", self.id);
+        self.status = DeviceState::Active;
+        let core_state = state.as_any_mut().downcast_mut::<Core>().unwrap();
+        self.step = core_state.step;
+
+        self.update_geo_data(core_state);
+        self.transfer_data_to_rsu(core_state);
+        self.transfer_data_to_bs(core_state);
+        self.transfer_data_to_vehicles(core_state);
+
+        // Initiate deactivation if it is time
+        if self.step == self.timing.peek_deactivation_time() {
+            self.deactivate(core_state);
+        }
+    }
+
     fn is_stopped(&mut self, _state: &mut dyn State) -> bool {
         false
     }
