b454b2fc92c72db74e010733af5147a44cc08826
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-09 22:31:55 +0200

    refactored: data module into reader module

--- src/data/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod data_io;
-mod df_handler;
-mod df_utils;
-mod file_io;
-pub(crate) mod stream_io;
---
+++ src/reader/mod.rs
@@ -0,0 +1,6 @@
+pub(crate) mod activation;
+mod df_handler;
+mod df_utils;
+mod files;
+pub(crate) mod geo;
+pub(crate) mod links;
