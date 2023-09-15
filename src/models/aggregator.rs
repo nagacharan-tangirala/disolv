2c20420e9179a97bfb93abfb48bd802d679f0ee1
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-09-15 23:15:54 +0200

    added: data category-wise data counts and sizes calculation

--- src/models/aggregator.rs
+++ src/models/aggregator.rs
@@ -1,6 +1,7 @@
 use crate::device::base_station::BSInfo;
 use crate::models::composer::DevicePayload;
 use crate::reader::activation::DeviceId;
+use crate::utils::ds_config::SensorType;
 use krabmaga::hashbrown::HashMap;
 
 #[derive(Clone, Debug, Copy)]
@@ -19,10 +20,10 @@ pub(crate) struct InfraPayload {
     pub(crate) rsu_count: usize,
     pub(crate) vehicle_ids: Vec<DeviceId>,
     pub(crate) rsu_ids: Vec<DeviceId>,
-    pub(crate) vehicle_data_size: f32,
-    pub(crate) rsu_data_size: f32,
-    pub(crate) vehicle_data_counts: u32,
-    pub(crate) rsu_data_counts: u32,
+    pub(crate) vehicle_data_size: HashMap<SensorType, f32>,
+    pub(crate) rsu_data_size: HashMap<SensorType, f32>,
+    pub(crate) vehicle_data_counts: HashMap<SensorType, u32>,
+    pub(crate) rsu_data_counts: HashMap<SensorType, u32>,
 }
 
 impl BasicAggregator {
@@ -39,14 +40,38 @@ impl BasicAggregator {
         bs_payload.rsu_count = rsu2bs_data.len();
 
         for payload in v2bs_data.iter() {
-            bs_payload.vehicle_data_size += payload.total_data_size;
-            bs_payload.vehicle_data_counts += payload.total_data_count;
             bs_payload.vehicle_ids.push(payload.id);
+            for (sensor_type, data_size) in payload.generated_data_size.iter() {
+                bs_payload
+                    .vehicle_data_size
+                    .entry(*sensor_type)
+                    .and_modify(|e| *e += *data_size)
+                    .or_insert(*data_size);
+            }
+            for (sensor_type, data_count) in payload.types_with_counts.iter() {
+                bs_payload
+                    .vehicle_data_counts
+                    .entry(*sensor_type)
+                    .and_modify(|e| *e += *data_count)
+                    .or_insert(*data_count);
+            }
         }
         for payload in rsu2bs_data.iter() {
-            bs_payload.rsu_data_size += payload.total_data_size;
-            bs_payload.rsu_data_counts += payload.total_data_count;
             bs_payload.rsu_ids.push(payload.id);
+            for (sensor_type, data_size) in payload.generated_data_size.iter() {
+                bs_payload
+                    .rsu_data_size
+                    .entry(*sensor_type)
+                    .and_modify(|e| *e += *data_size)
+                    .or_insert(*data_size);
+            }
+            for (sensor_type, data_count) in payload.types_with_counts.iter() {
+                bs_payload
+                    .rsu_data_counts
+                    .entry(*sensor_type)
+                    .and_modify(|e| *e += *data_count)
+                    .or_insert(*data_count);
+            }
         }
         return bs_payload;
     }
