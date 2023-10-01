fd07795fe1a9694a6243ba6936c463c42c8a764f
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-01 21:02:20 +0200

    modified: the position of the impl blocks

--- pavenet-io/src/input/links.rs
+++ pavenet-io/src/input/links.rs
@@ -19,6 +19,53 @@ pub struct LinksReader {
     streaming_interval: TimeStamp,
 }
 
+impl LinksReader {
+    pub(crate) fn builder(trace_file: &PathBuf) -> LinksReaderBuilder {
+        LinksReaderBuilder::new(trace_file.clone())
+    }
+
+    pub fn links_data(&self, step: TimeStamp) -> Result<LinkMapType, Box<dyn Error>> {
+        if self.is_stream != true && self.link_type == LinkType::Single {
+            return self.read_single_link_map();
+        }
+        if self.is_stream != true && self.link_type == LinkType::Multiple {
+            return self.read_multiple_link_map();
+        }
+        return self.stream_multiple_link_map(step);
+    }
+
+    fn read_single_link_map(&self) -> Result<LinkMapType, Box<dyn Error>> {
+        let links_df = files::read_file(&self.links_file)?;
+        let links_map: LinkMap =
+            dfs::extract_single_links(&links_df, &self.device_column, &self.neighbour_column)?;
+        return Ok(LinkMapType::Single(links_map));
+    }
+
+    fn read_multiple_link_map(&self) -> Result<LinkMapType, Box<dyn Error>> {
+        let links_df = files::read_file(&self.links_file)?;
+        let links_map: MultiLinkMap =
+            dfs::extract_multiple_links(&links_df, &self.device_column, &self.neighbour_column)?;
+        return Ok(LinkMapType::Multiple(links_map));
+    }
+
+    fn stream_multiple_link_map(&self, step: TimeStamp) -> Result<LinkMapType, Box<dyn Error>> {
+        let starting_time: TimeStamp = step;
+        let ending_time: TimeStamp = step + self.streaming_interval;
+        return if self.is_stream == true {
+            let links_df: DataFrame =
+                files::stream_parquet_in_interval(&self.links_file, starting_time, ending_time)?;
+            let static_links: TraceLinkMap =
+                dfs::extract_link_traces(&links_df, &self.device_column, &self.neighbour_column)?;
+            Ok(LinkMapType::LinkTraceMap(static_links))
+        } else {
+            let links_df = files::read_file(&self.links_file)?;
+            let static_links: TraceLinkMap =
+                dfs::extract_link_traces(&links_df, &self.device_column, &self.neighbour_column)?;
+            Ok(LinkMapType::LinkTraceMap(static_links))
+        };
+    }
+}
+
 #[derive(Default)]
 pub struct LinksReaderBuilder {
     links_file: PathBuf,
@@ -73,52 +120,5 @@ impl LinksReaderBuilder {
             link_type: self.link_type,
             streaming_interval: self.streaming_interval,
         }
-    }
-}
-
-impl LinksReader {
-    pub(crate) fn builder(trace_file: &PathBuf) -> LinksReaderBuilder {
-        LinksReaderBuilder::new(trace_file.clone())
-    }
-
-    pub fn links_data(&self, step: TimeStamp) -> Result<LinkMapType, Box<dyn Error>> {
-        if self.is_stream != true && self.link_type == LinkType::Single {
-            return self.read_single_link_map();
-        }
-        if self.is_stream != true && self.link_type == LinkType::Multiple {
-            return self.read_multiple_link_map();
-        }
-        return self.stream_multiple_link_map(step);
-    }
-
-    fn read_single_link_map(&self) -> Result<LinkMapType, Box<dyn Error>> {
-        let links_df = files::read_file(&self.links_file)?;
-        let links_map: LinkMap =
-            dfs::extract_single_links(&links_df, &self.device_column, &self.neighbour_column)?;
-        return Ok(LinkMapType::Single(links_map));
-    }
-
-    fn read_multiple_link_map(&self) -> Result<LinkMapType, Box<dyn Error>> {
-        let links_df = files::read_file(&self.links_file)?;
-        let links_map: MultiLinkMap =
-            dfs::extract_multiple_links(&links_df, &self.device_column, &self.neighbour_column)?;
-        return Ok(LinkMapType::Multiple(links_map));
-    }
-
-    fn stream_multiple_link_map(&self, step: TimeStamp) -> Result<LinkMapType, Box<dyn Error>> {
-        let starting_time: TimeStamp = step;
-        let ending_time: TimeStamp = step + self.streaming_interval;
-        return if self.is_stream == true {
-            let links_df: DataFrame =
-                files::stream_parquet_in_interval(&self.links_file, starting_time, ending_time)?;
-            let static_links: TraceLinkMap =
-                dfs::extract_link_traces(&links_df, &self.device_column, &self.neighbour_column)?;
-            Ok(LinkMapType::LinkTraceMap(static_links))
-        } else {
-            let links_df = files::read_file(&self.links_file)?;
-            let static_links: TraceLinkMap =
-                dfs::extract_link_traces(&links_df, &self.device_column, &self.neighbour_column)?;
-            Ok(LinkMapType::LinkTraceMap(static_links))
-        };
     }
 }
