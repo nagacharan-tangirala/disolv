6581003d14ccf09a316145a0b13535d0ae9ac918
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-04 09:20:16 +0200

    simplified: node trait method requirements are reduced

--- pavenet-core/src/node/node.rs
+++ pavenet-core/src/node/node.rs
@@ -3,16 +3,13 @@ use crate::node::info::NodeInfo;
 use crate::node::power::PowerState;
 use crate::node::receive::Recipient;
 use crate::node::transmit::Transmitter;
-use krabmaga::engine::agent::Agent;
-use pavenet_config::types::ts::TimeStamp;
+use downcast_rs::Downcast;
+use dyn_clone::DynClone;
 
-pub trait Node: Agent + Transmitter + Recipient + Copy + Send + Sync + Clone {
-    fn as_agent(self) -> Box<dyn Agent>;
+pub trait Node: Transmitter + Recipient + Send + Sync + Downcast + DynClone {
+    fn power_state(&self) -> PowerState;
     fn node_info(&self) -> NodeInfo;
-    fn power_on_time(&self) -> TimeStamp;
-    fn set_power(&mut self, power_state: PowerState);
-    fn update_map_state(&mut self, core: &mut Core);
-    fn transmit_data(&mut self, core: &mut Core);
-    fn receive_data(&mut self, core: &mut Core);
-    fn collect_stats(&mut self, core: &mut Core);
+    fn set_power_state(&mut self, power_state: PowerState);
+    fn step(&mut self, core: &mut Core);
+    fn after_step(&mut self, core: &mut Core);
 }
