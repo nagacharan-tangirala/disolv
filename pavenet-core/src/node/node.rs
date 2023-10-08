266ad81c50bddf2c0756931a14f057e5b0557a4d
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-08 16:28:20 +0200

    added: node order as a trait fn, required for scheduling

--- pavenet-core/src/node/node.rs
+++ pavenet-core/src/node/node.rs
@@ -1,5 +1,4 @@
 use crate::core::core::Core;
-use crate::node::info::NodeInfo;
 use crate::node::power::PowerState;
 use crate::node::receive::Recipient;
 use crate::node::transmit::Transmitter;
@@ -8,7 +7,7 @@ use dyn_clone::DynClone;
 
 pub trait Node: Transmitter + Recipient + Send + Sync + Downcast + DynClone {
     fn power_state(&self) -> PowerState;
-    fn node_info(&self) -> NodeInfo;
+    fn node_order(&self) -> i32;
     fn set_power_state(&mut self, power_state: PowerState);
     fn step(&mut self, core: &mut Core);
     fn after_step(&mut self, core: &mut Core);
