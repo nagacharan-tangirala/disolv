234bdf937072292d59739d9441743a4e1f935dc6
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-10 22:45:11 +0200

    modified: responder to follow new traits

--- pavenet-models/src/node/responder.rs
+++ pavenet-models/src/node/responder.rs
@@ -1,20 +1,36 @@
-use crate::node::composer::Payload;
-use crate::reader::activation::DeviceId;
-use krabmaga::hashbrown::HashMap;
-use pavenet_config::config::types::DeviceId;
+use crate::model::{NodeModel, TomlReadable};
+use crate::node::payload::Payload;
+use hashbrown::HashMap;
+use pavenet_core::types::NodeId;
 use rand::prelude::SliceRandom;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Responder {
+    pub name: String,
+}
+
+impl TomlReadable for Responder {}
 
 #[derive(Clone, Debug, Copy)]
-pub(crate) enum ResponderType {
+pub enum ResponderType {
     Stats(StatsResponder),
 }
 
 #[derive(Clone, Debug, Copy)]
 pub(crate) struct StatsResponder;
 
+impl NodeModel for StatsResponder {
+    type Input = Responder;
+    fn to_input(&self) -> Responder {
+        let name: String = "stats".to_string();
+        Responder { name }
+    }
+}
+
 #[derive(Clone, Debug, Default)]
 pub(crate) struct DownlinkPayload {
-    pub(crate) id: DeviceId,
+    pub(crate) id: NodeId,
     pub(crate) latency_factor: u32,
 }
 
@@ -27,13 +43,15 @@ impl StatsResponder {
         &self,
         veh_payloads: &Vec<Payload>,
         rsu_counts: usize,
-    ) -> HashMap<DeviceId, DownlinkPayload> {
-        let mut veh_ids: Vec<DeviceId> = veh_payloads.iter().map(|p| p.id).collect();
+    ) -> HashMap<NodeId, DownlinkPayload> {
+        let mut veh_ids: Vec<NodeId> = veh_payloads
+            .iter()
+            .map(|p| p.sensor_data.node_info.id)
+            .collect();
         veh_ids.shuffle(&mut rand::thread_rng());
         let crowd_latency = rsu_counts + veh_ids.len();
 
-        let mut responses: HashMap<DeviceId, DownlinkPayload> =
-            HashMap::with_capacity(veh_ids.len());
+        let mut responses: HashMap<NodeId, DownlinkPayload> = HashMap::with_capacity(veh_ids.len());
         for (idx, veh_id) in veh_ids.iter().enumerate() {
             let response = DownlinkPayload {
                 id: *veh_id,
