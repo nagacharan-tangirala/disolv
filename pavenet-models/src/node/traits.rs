c74a7352c35086bcfe1935aba75bf16136155b27
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-10 22:45:42 +0200

    added: traits that all the devices can utilize

---
+++ pavenet-models/src/node/traits.rs
@@ -0,0 +1,20 @@
+use pavenet_core::structs::MapState;
+use pavenet_engine::engine::core::Core;
+
+pub trait Mapper {
+    fn map_state(&self) -> MapState;
+    fn set_map_state(&mut self, map_state: MapState);
+}
+
+pub trait Transmitter {
+    type Item;
+    fn collect_downstream(&mut self) -> Vec<Self::Item>;
+    fn generate_data(&mut self, core: &mut Core) -> Self::Item;
+    fn transmit(&mut self, data: Self::Item);
+}
+
+pub trait Recipient {
+    type Item;
+    fn receive(&mut self, data: &Vec<Self::Item>);
+    fn report_stats(&mut self, core: &mut Core);
+}
