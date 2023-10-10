467ca43276bb4dca71875d896bb57d959d5d82eb
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-10 22:38:51 +0200

    modified: composer to follow new traits, simplified conversions

--- pavenet-models/src/node/composer.rs
+++ pavenet-models/src/node/composer.rs
@@ -1,7 +1,40 @@
-use pavenet_config::config::base::{ComposerSettings, DataType, SourceSettings};
-use pavenet_config::types::ids::node::NodeId;
+use crate::model::{NodeModel, TomlReadable};
+use crate::node::payload::{Payload, SensorData};
+use pavenet_core::enums::{DataType, NodeType};
+use serde::Deserialize;
 
 pub const SOURCE_SIZE: usize = 10;
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub struct DataSource {
+    pub data_type: DataType,
+    pub target_type: NodeType,
+    pub data_count: u32,
+    pub unit_size: f32,
+    pub frequency: u32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Composer {
+    pub name: String,
+    pub source_settings: Vec<DataSource>,
+}
+
+impl TomlReadable for Composer {}
+
+impl Composer {
+    pub fn ds_array(&self) -> [Option<DataSource>; SOURCE_SIZE] {
+        let mut data_sources = [None; SOURCE_SIZE];
+        for idx in 0..self.source_settings.len() {
+            data_sources[idx] = Some(self.source_settings[idx]);
+        }
+        return data_sources;
+    }
+
+    pub fn ds_count(&self) -> usize {
+        self.source_settings.len()
+    }
+}
 
 #[derive(Clone, Debug, Copy)]
 pub enum ComposerType {
@@ -11,83 +44,65 @@ pub enum ComposerType {
 
 #[derive(Clone, Debug, Copy)]
 pub struct BasicComposer {
-    pub data_sources: [Option<SourceSettings>; SOURCE_SIZE],
+    pub data_sources: [Option<DataSource>; SOURCE_SIZE],
     pub ds_count: usize,
 }
 
-#[derive(Clone, Debug, Default)]
-pub struct Payload {
-    pub sensor_data: SensorData,
-    pub total_size: f32,
-    pub total_count: u32,
-    pub downstream_data: Vec<NodeId>,
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct SensorData {
-    pub device_info: NodeInfo,
-    pub map_state: MapState,
-    pub size_by_type: HashMap<DataType, f32>,
-}
-
-#[derive(Clone, Debug, Copy)]
-pub struct StatusComposer {
-    data_sources: [Option<SourceSettings>; SOURCE_SIZE],
-    ds_count: usize,
+impl NodeModel for BasicComposer {
+    type Input = Composer;
+    fn to_input(&self) -> Composer {
+        let ds: Vec<DataSource> = self.data_sources.into_iter().flatten().collect();
+        let name: String = "basic".to_string();
+        Composer {
+            name,
+            source_settings: ds,
+        }
+    }
 }
 
 impl BasicComposer {
-    pub fn new(composer_settings: &ComposerSettings) -> Self {
-        let ds_settings = &composer_settings.source_settings;
-        let mut data_sources = [None; SOURCE_SIZE];
-        for idx in 0..ds_settings.len() {
-            data_sources[idx] = Some(composer_settings.source_settings[idx]);
-        }
+    pub fn new(composer_settings: &Composer) -> Self {
         Self {
-            data_sources,
-            ds_count: ds_settings.len(),
+            data_sources: composer_settings.ds_array(),
+            ds_count: composer_settings.ds_count(),
         }
     }
 
-    pub(crate) fn update_data_sources(&mut self, composer_settings: &ComposerSettings) {
-        let ds_settings = &composer_settings.source_settings;
-        self.data_sources = [None; SOURCE_SIZE];
-        for idx in 0..ds_settings.len() {
-            self.data_sources[idx] = Some(ds_settings[idx]);
-        }
-        self.ds_count = ds_settings.len();
-    }
-
-    pub(crate) fn compose_sensor_data(&self) -> SensorData {
+    pub(crate) fn compose_sensor_data(&self, target_type: NodeType) -> SensorData {
         let mut sensor_data = SensorData::default();
-        sensor_data.temperature = 25.0;
-        sensor_data.env_temperature = 32.0;
         for idx in 0..self.ds_count {
-            // Unwrap is safe here because type counts is the length of the array.
             let ds_settings = self.data_sources[idx].unwrap();
+            if ds_settings.target_type != target_type {
+                continue;
+            }
             let data_type = ds_settings.data_type;
             let data_counts = ds_settings.data_count;
             let data_size = ds_settings.unit_size * data_counts as f32;
-
             sensor_data.size_by_type.insert(data_type, data_size);
             sensor_data.count_by_type.insert(data_type, data_counts);
         }
         return sensor_data;
     }
 
-    pub(crate) fn compose_payload(&self, incoming_payload: Option<Vec<Payload>>) -> Payload {
+    pub(crate) fn compose_payload(
+        &self,
+        target_type: NodeType,
+        incoming_payload: Option<Vec<Payload>>,
+    ) -> Payload {
         let mut payload = Payload::default();
-        payload.sensor_data = self.compose_sensor_data();
+        payload.sensor_data = self.compose_sensor_data(target_type);
         payload.total_size = payload.sensor_data.size_by_type.values().sum();
-        payload.total_count = payload.sensor_data.size_by_type.values().sum();
+        payload.total_count = payload.sensor_data.count_by_type.values().sum();
 
         match incoming_payload {
-            Some(incoming_payload) => {
-                payload
-                    .downstream_data
-                    .append(incoming_payload.iter().map(|x| x.sensor_data).collect());
-                payload.total_size += incoming_payload.iter().map(|x| x.total_size).sum();
-                payload.total_count += incoming_payload.iter().map(|x| x.total_count).sum();
+            Some(mut payload_in) => {
+                for single_payload in payload_in.iter_mut() {
+                    payload
+                        .downstream_data
+                        .append(&mut single_payload.downstream_data);
+                    payload.total_size += single_payload.total_size;
+                    payload.total_count += single_payload.total_count;
+                }
             }
             None => {}
         };
@@ -95,34 +110,39 @@ impl BasicComposer {
     }
 }
 
-impl StatusComposer {
-    pub fn new(composer_settings: &ComposerSettings) -> Self {
-        let ds_settings = &composer_settings.source_settings;
-        let mut data_sources = [None; SOURCE_SIZE];
-        for idx in 0..ds_settings.len() {
-            data_sources[idx] = Some(composer_settings.source_settings[idx]);
+#[derive(Clone, Debug, Copy)]
+pub struct StatusComposer {
+    data_sources: [Option<DataSource>; SOURCE_SIZE],
+    ds_count: usize,
+}
+
+impl NodeModel for StatusComposer {
+    type Input = Composer;
+    fn to_input(&self) -> Composer {
+        let ds: Vec<DataSource> = self.data_sources.into_iter().flatten().collect();
+        let name: String = "basic".to_string();
+        Composer {
+            name,
+            source_settings: ds,
         }
-        let ds_count = ds_settings.len();
+    }
+}
+
+impl StatusComposer {
+    pub fn new(composer_settings: &Composer) -> Self {
         Self {
-            data_sources,
-            ds_count,
+            data_sources: composer_settings.ds_array(),
+            ds_count: composer_settings.ds_count(),
         }
     }
 
-    pub(crate) fn update_data_sources(&mut self, composer_settings: &ComposerSettings) {
-        let ds_settings = &composer_settings.source_settings;
-        self.data_sources = [None; SOURCE_SIZE];
-        for idx in 0..ds_settings.len() {
-            self.data_sources[idx] = Some(ds_settings[idx]);
-        }
-        self.ds_count = ds_settings.len();
+    pub(crate) fn update_data_sources(&mut self, composer_settings: &Composer) {
+        self.data_sources = composer_settings.ds_array();
+        self.ds_count = composer_settings.ds_count();
     }
 
     pub(crate) fn compose_sensor_data(&self) -> SensorData {
         let mut sensor_data = SensorData::default();
-        sensor_data.temperature = 25.0;
-        sensor_data.env_temperature = 32.0;
-
         let data_size = 0.01;
         let data_count: u32 = 10;
 
@@ -137,15 +157,17 @@ impl StatusComposer {
         let mut payload = Payload::default();
         payload.sensor_data = self.compose_sensor_data();
         payload.total_size = payload.sensor_data.size_by_type.values().sum();
-        payload.total_count = payload.sensor_data.size_by_type.values().sum();
+        payload.total_count = payload.sensor_data.count_by_type.values().sum();
 
         match incoming_payload {
-            Some(incoming_payload) => {
-                payload
-                    .downstream_data
-                    .append(incoming_payload.iter().map(|x| x.sensor_data).collect());
-                payload.total_size += incoming_payload.iter().map(|x| x.total_size).sum();
-                payload.total_count += incoming_payload.iter().map(|x| x.total_count).sum();
+            Some(mut payload_in) => {
+                for single_payload in payload_in.iter_mut() {
+                    payload
+                        .downstream_data
+                        .append(&mut single_payload.downstream_data);
+                    payload.total_size += single_payload.total_size;
+                    payload.total_count += single_payload.total_count;
+                }
             }
             None => {}
         };
