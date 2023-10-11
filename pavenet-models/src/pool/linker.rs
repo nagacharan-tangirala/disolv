8bae3a24f3f51c8cfd8e0ec88fb24843d0d0590d
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-11 21:31:57 +0200

    modified: linker to node_links to support multi-link comms for devices

--- pavenet-models/src/pool/linker.rs
+++ pavenet-models/src/pool/linker.rs
@@ -1,15 +1,11 @@
 use crate::model::PoolModel;
 use crate::node::payload::Payload;
 use hashbrown::HashMap;
-use log::error;
 use pavenet_core::enums::{NodeType, TransferMode};
 use pavenet_core::structs::Link;
-use pavenet_core::types::{NodeId, TimeStamp};
+use pavenet_core::types::{NodeId, Order, TimeStamp};
 use pavenet_input::input::links::{LinkMap, LinkReaderType, LinksFetcher};
 use serde::Deserialize;
-use typed_builder::TypedBuilder;
-
-pub const LINKER_SIZE: usize = 10;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct LinkConfig {
@@ -20,59 +16,93 @@ pub struct LinkConfig {
     pub is_streaming: bool,
 }
 
-#[derive(TypedBuilder)]
-pub struct Linker {
-    pub name: String,
+#[derive(Deserialize, Debug, Clone)]
+pub struct LinkerSettings {
     pub link_config: Vec<LinkConfig>,
+}
+
+pub struct Linker {
+    pub link_config: LinkConfig,
     pub links: LinkMap,
     pub reader: LinkReaderType,
-    #[builder(default)]
     pub link_cache: HashMap<NodeId, Link>,
-    #[builder(default)]
     pub uplink: HashMap<NodeId, Vec<Payload>>,
-    #[builder(default)]
     pub downlink: HashMap<NodeId, Vec<Payload>>,
 }
+
+impl Linker {}
 
 impl PoolModel for Linker {
     fn init(&mut self, step: TimeStamp) {
         self.links = match self.reader {
-            LinkReaderType::File(ref mut reader) => match reader.fetch_links_data(step) {
-                Ok(map) => map,
-                Err(e) => {
-                    error!("Error reading map state: {}", e);
-                    HashMap::new()
-                }
-            },
-            LinkReaderType::Stream(ref mut reader) => match reader.fetch_links_data(step) {
-                Ok(map) => map,
-                Err(e) => {
-                    error!("Error reading map state: {}", e);
-                    HashMap::new()
-                }
-            },
+            LinkReaderType::File(ref mut reader) => {
+                reader.fetch_links_data(step).unwrap_or_default()
+            }
+            LinkReaderType::Stream(ref mut reader) => {
+                reader.fetch_links_data(step).unwrap_or_default()
+            }
         };
     }
 
     fn stream_data(&mut self, step: TimeStamp) {
         match self.reader {
             LinkReaderType::Stream(ref mut reader) => {
-                self.links = match reader.fetch_links_data(step) {
-                    Ok(map) => map,
-                    Err(e) => {
-                        error!("Error reading map state: {}", e);
-                        HashMap::new()
-                    }
-                };
+                self.links = reader.fetch_links_data(step).unwrap_or_default()
             }
             _ => {}
         }
     }
 
     fn refresh_cache(&mut self, step: TimeStamp) {
-        self.link_cache = match self.links.remove(&step) {
-            Some(traces) => traces,
-            None => HashMap::new(),
-        };
+        self.link_cache = self.links.remove(&step).unwrap_or_default();
+    }
+}
+
+pub struct NodeLinks {
+    target_type_links: Vec<(NodeType, Linker)>,
+}
+
+impl NodeLinks {
+    pub fn new(node_type_links: HashMap<NodeType, Linker>) -> Self {
+        let mut target_type_links = Vec::with_capacity(node_type_links.len());
+        for (node_type, links) in node_type_links {
+            target_type_links.push((node_type, links));
+        }
+        Self { target_type_links }
+    }
+
+    pub fn links_for(&mut self, node_id: NodeId, node_type: NodeType) -> Link {
+        self.linker_for(node_type)
+            .link_cache
+            .remove(&node_id)
+            .unwrap_or_default()
+    }
+
+    fn linker_for(&mut self, node_type: NodeType) -> &mut Linker {
+        self.target_type_links
+            .iter_mut()
+            .find(|(target_type, _)| *target_type == node_type)
+            .map(|(_, links)| links)
+            .unwrap_or_else(|| panic!("No links found for node type: {:?}", node_type))
+    }
+}
+
+impl PoolModel for NodeLinks {
+    fn init(&mut self, step: TimeStamp) {
+        self.target_type_links.iter_mut().for_each(|(_, links)| {
+            links.init(step);
+        });
+    }
+
+    fn stream_data(&mut self, step: TimeStamp) {
+        self.target_type_links.iter_mut().for_each(|(_, links)| {
+            links.stream_data(step);
+        });
+    }
+
+    fn refresh_cache(&mut self, step: TimeStamp) {
+        self.target_type_links.iter_mut().for_each(|(_, links)| {
+            links.refresh_cache(step);
+        });
     }
 }
