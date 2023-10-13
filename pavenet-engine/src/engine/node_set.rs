dd40ee873213bf7092ea98bf32b2804b0132e8d3
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-13 23:27:06 +0200

    modified: node_set is converted to be generic on Node

--- pavenet-engine/src/engine/node_set.rs
+++ pavenet-engine/src/engine/node_set.rs
@@ -1,34 +1,37 @@
 use crate::engine::nodeimpl::NodeImpl;
 use crate::node::node::Node;
-use crate::node::power::{PowerSchedule, PowerState};
+use crate::node::pool::NodePool;
+use crate::node::power::PowerState;
 use hashbrown::HashMap;
 use krabmaga::engine::schedule::Schedule;
-use pavenet_core::named::ids::node::NodeId;
+use krabmaga::log;
+use pavenet_core::types::NodeId;
 
-#[derive(Default)]
-pub struct PoolImpl {
-    nodes_by_id: HashMap<NodeId, NodeImpl>,
+#[derive(Default, Clone)]
+pub struct NodeSet<T, U>
+where
+    T: Node,
+    U: NodePool,
+{
+    pub(crate) nodes_by_id: HashMap<NodeId, NodeImpl<T, U>>,
     pub to_add: Vec<NodeId>,
     pub to_pop: Vec<NodeId>,
 }
 
-impl PoolImpl {
-    pub fn new(
-        dyn_nodes: HashMap<NodeId, Box<dyn Node>>,
-        power_schedule_map: HashMap<NodeId, PowerSchedule>,
-    ) -> Self {
-        let mut by_id: HashMap<NodeId, NodeImpl> = HashMap::new();
-        for (node_id, dyn_node) in dyn_nodes.into_iter() {
-            let node = NodeImpl::new(node_id, power_schedule_map[&node_id], dyn_node);
-            by_id.insert(node_id, node);
-        }
+impl<T, U> NodeSet<T, U>
+where
+    T: Node,
+    U: NodePool,
+{
+    pub fn new(nodes: HashMap<NodeId, NodeImpl<T, U>>) -> Self {
         Self {
-            nodes_by_id: by_id,
+            nodes_by_id: nodes,
             ..Default::default()
         }
     }
 
     pub(crate) fn init(&mut self) {
+        log!(LogType::Info, String::from("NodeSet::init"));
         self.to_add.extend(self.nodes_by_id.keys());
     }
 
@@ -37,7 +40,7 @@ impl PoolImpl {
             match self.nodes_by_id.get_mut(node_id) {
                 Some(node) => {
                     schedule.schedule_repeating(
-                        node.as_agent(),
+                        Box::new(node.clone()),
                         node.node_id.as_u32(),
                         node.power_schedule.pop_time_to_on().as_f32(),
                         node.node.node_order(),
@@ -53,7 +56,7 @@ impl PoolImpl {
             match self.nodes_by_id.get_mut(node_id) {
                 Some(node) => {
                     node.node.set_power_state(PowerState::Off);
-                    schedule.dequeue(node.clone().as_agent(), (*node_id).into());
+                    schedule.dequeue(Box::new(node.clone()), (*node_id).into());
                 }
                 None => panic!("Could not find node {}", node_id),
             }
@@ -66,19 +69,9 @@ mod tests {
     use super::*;
     use crate::engine::nodeimpl::tests::*;
 
-    pub(crate) fn make_node_pool() -> PoolImpl {
-        let nodes = make_dyn_nodes();
-        let mut power_schedule_map: HashMap<NodeId, PowerSchedule> = HashMap::new();
-        for i in 0..10 {
-            let power_schedule = make_power_schedule();
-            power_schedule_map.insert(i.into(), power_schedule);
-        }
-        PoolImpl::new(nodes, power_schedule_map)
-    }
-
     #[test]
     fn add_to_schedule() {
-        let mut nodes = make_node_pool();
+        let mut nodes = make_test_node_set();
         let mut schedule = Schedule::new();
         nodes.to_add.push(1.into());
         nodes.power_on(&mut schedule);
@@ -87,7 +80,7 @@ mod tests {
 
     #[test]
     fn pop_from_schedule() {
-        let mut nodes = make_node_pool();
+        let mut nodes = make_test_node_set();
         let mut schedule = Schedule::new();
         nodes.to_pop.push(1.into());
         nodes.power_off(&mut schedule);
@@ -96,7 +89,7 @@ mod tests {
 
     #[test]
     fn power_off() {
-        let mut nodes = make_node_pool();
+        let mut nodes = make_test_node_set();
         let mut schedule = Schedule::new();
         nodes.to_pop.push(1.into());
         nodes.power_off(&mut schedule);
@@ -110,7 +103,7 @@ mod tests {
 
     #[test]
     fn init() {
-        let mut nodes = make_node_pool();
+        let mut nodes = make_test_node_set();
         nodes.init();
         assert_eq!(nodes.to_add.len(), 10);
     }
