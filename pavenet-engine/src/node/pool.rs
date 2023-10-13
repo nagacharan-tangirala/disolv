0329736282554c31ea647bc634cf1032a6f03b08
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-13 23:24:56 +0200

    added: useful constraints on the NodePool trait

--- pavenet-engine/src/node/pool.rs
+++ pavenet-engine/src/node/pool.rs
@@ -1,7 +1,7 @@
 use krabmaga::engine::schedule::Schedule;
 use pavenet_core::named::ts::TimeStamp;
 
-pub trait NodePool: Send + Sync {
+pub trait NodePool: Clone + Send + Sync + Default + 'static {
     fn init(&mut self, schedule: &mut Schedule);
     fn before_step(&mut self, step: TimeStamp);
     fn update(&mut self, step: TimeStamp);
