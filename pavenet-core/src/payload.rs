4f8fe2a9d7fb842c10b9ab4272e537fdabab4477
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-17 16:34:24 +0200

    added: impl block to build the payload instance

--- pavenet-core/src/payload.rs
+++ pavenet-core/src/payload.rs
@@ -15,22 +15,22 @@ where
 }
 
 #[derive(Clone, Default)]
-pub struct PayloadData<I, D, Q>
+pub struct PayloadData<D, I, Q>
 where
+    D: DataCreek<Q>,
     I: Identifier,
     Q: Queryable,
-    D: DataCreek<Q>,
 {
     pub data_pile: D, // consolidate after gathering
     pub from_node: I,
     _phantom: std::marker::PhantomData<fn() -> Q>,
 }
 
-impl<I, D, Q> PayloadData<I, D, Q>
+impl<I, D, Q> PayloadData<D, I, Q>
 where
+    D: DataCreek<Q>,
     I: Identifier,
     Q: Queryable,
-    D: DataCreek<Q>,
 {
     pub fn new(data_pile: D, from_node: I) -> Self {
         Self {
@@ -42,15 +42,36 @@ where
 }
 
 #[derive(Clone, Default)]
-pub struct Payload<D, P, I, Q>
+pub struct Payload<D, I, P, Q>
 where
     D: DataCreek<Q>,
-    P: PayloadStats<D, Q>,
     I: Identifier,
+    P: PayloadStats<D, Q>,
     Q: Queryable,
 {
-    pub gathered_data: Option<Vec<PayloadData<I, D, Q>>>,
-    pub data_pile: PayloadData<I, D, Q>,
+    pub gathered_data: Option<Vec<PayloadData<D, I, Q>>>,
+    pub data_pile: PayloadData<D, I, Q>,
     pub payload_stats: P,
     pub _phantom: std::marker::PhantomData<fn() -> (D, I, Q)>,
+}
+
+impl<D, I, P, Q> Payload<D, I, P, Q>
+where
+    D: DataCreek<Q>,
+    I: Identifier,
+    P: PayloadStats<D, Q>,
+    Q: Queryable,
+{
+    pub fn new(data_pile: PayloadData<D, I, Q>, payload_stats: P) -> Self {
+        Self {
+            gathered_data: None,
+            data_pile,
+            payload_stats,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+
+    pub fn add_gathered_data(&mut self, gathered_data: Option<Vec<PayloadData<D, I, Q>>>) {
+        self.gathered_data = gathered_data;
+    }
 }
