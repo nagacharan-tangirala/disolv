148f9a9a8e96dd8e1a728399400499fea3629177
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-17 22:48:36 +0200

    added: initial version of node finder trait

--- pavenet-core/src/node_finder.rs
+++ pavenet-core/src/node_finder.rs
@@ -1,9 +1,31 @@
 use pavenet_engine::entity::{Identifier, Kind};
 
-trait NodeFinder<K, I>
+pub trait LinkInfo<I> {}
+
+pub struct NodeLink<I, L>
 where
-    K: Kind,
     I: Identifier,
+    L: LinkInfo<I>,
 {
-    fn nodes_of_kind(&self, kind: K) -> Option<&Vec<I>>;
+    pub target: Vec<I>,
+    pub link_info: L,
+}
+
+impl<I, L> NodeLink<I, L>
+where
+    I: Identifier,
+    L: LinkInfo<I>,
+{
+    pub fn new(target: Vec<I>, link_info: L) -> Self {
+        Self { target, link_info }
+    }
+}
+
+trait NodeFinder<I, K, L>
+where
+    I: Identifier,
+    K: Kind,
+    L: LinkInfo<I>,
+{
+    fn nodes_of_kind(&self, kind: K) -> Option<&Vec<NodeLink<I, L>>>;
 }
