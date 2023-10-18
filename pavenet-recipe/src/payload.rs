7e4a64d5bebd9b2c9579d61cff9f5ddf42a7299e
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-18 15:56:21 +0200

    fixed: payload definition in the recipe

--- pavenet-recipe/src/payload.rs
+++ pavenet-recipe/src/payload.rs
@@ -1,8 +1,13 @@
 use crate::mobility::MapState;
-use crate::node::id::NodeId;
-use crate::node::NodeInfo;
+use crate::node_info::id::NodeId;
+use crate::node_info::NodeInfo;
 use hashbrown::HashMap;
+use pavenet_core::download::Queryable;
+use pavenet_core::upload::{DataCreek, Payload, PayloadData, PayloadMetadata};
 use serde_derive::Deserialize;
+
+pub type TPayloadData = PayloadData<PayloadContent, NodeId, DataType>;
+pub type TPayload = Payload<PayloadContent, NodeId, PayloadStats, DataType>;
 
 #[derive(Deserialize, Debug, Hash, Copy, Clone, PartialEq, Eq)]
 pub enum DataType {
@@ -14,18 +19,22 @@ pub enum DataType {
     Status,
 }
 
-#[derive(Clone, Debug, Default)]
-pub struct Payload {
-    pub sensor_data: SensorData,
-    pub total_size: f32,
-    pub total_count: u32,
-    pub downstream_data: Vec<NodeId>,
-}
+impl Queryable for DataType {}
 
-#[derive(Clone, Debug, Default)]
-pub struct SensorData {
+#[derive(Copy, Clone, Debug, Default)]
+pub struct PayloadContent {
     pub node_info: NodeInfo,
     pub map_state: MapState,
+}
+
+impl DataCreek<DataType> for PayloadContent {}
+
+#[derive(Clone, Debug, Default)]
+pub struct PayloadStats {
+    pub total_size: f32,
+    pub total_count: u32,
     pub size_by_type: HashMap<DataType, f32>,
     pub count_by_type: HashMap<DataType, u32>,
 }
+
+impl PayloadMetadata<PayloadContent, DataType> for PayloadStats {}
