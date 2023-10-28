373326bd1b0b534d2ba145e7d23b3444f31605ae
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-28 13:39:01 +0200

    removed: reexporting krabmaga scheduler

--- pavenet-engine/src/lib.rs
+++ pavenet-engine/src/lib.rs
@@ -1,10 +1,9 @@
 #![forbid(unsafe_code)]
 pub mod bucket;
+pub mod channel;
 pub mod engine;
 pub mod entity;
 pub mod node;
 pub mod payload;
 pub mod response;
 pub mod scheduler;
-
-pub use krabmaga::engine::schedule::Schedule;
