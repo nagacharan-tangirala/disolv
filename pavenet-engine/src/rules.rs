137234d0a8759f58f001b87ae431f11bc45592b6
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-30 21:07:48 +0100

    modified: cascading changes due to payload and tier dependencies

--- pavenet-engine/src/rules.rs
+++ pavenet-engine/src/rules.rs
@@ -1,70 +1,63 @@
-use crate::entity::{Kind, Tier};
+use crate::entity::Tier;
 use crate::payload::{GPayload, PayloadContent, PayloadMetadata};
 use crate::response::Queryable;
 use hashbrown::HashMap;
 
 /// A trait that represents the type of a rule.
-pub trait RuleAction<K, T>: Default + Copy + Clone + Send + Sync
+pub trait RuleAction<T>: Default + Copy + Clone + Send + Sync
 where
-    K: Kind,
     T: Tier,
 {
 }
 
 #[derive(Clone, Debug)]
-pub struct GTxRule<K, Q, R, T>
+pub struct GTxRule<Q, R, T>
 where
-    K: Kind,
     Q: Queryable,
-    R: RuleAction<K, T>,
+    R: RuleAction<T>,
     T: Tier,
 {
-    pub data_source: K,
+    pub data_source: T,
     pub query_type: Q,
     pub action: R,
-    _phantom: std::marker::PhantomData<fn() -> T>,
 }
 
-impl<K, Q, R, T> GTxRule<K, Q, R, T>
+impl<Q, R, T> GTxRule<Q, R, T>
 where
-    K: Kind,
     Q: Queryable,
-    R: RuleAction<K, T>,
+    R: RuleAction<T>,
     T: Tier,
 {
-    pub fn new(data_source: K, query_type: Q, rule_type: R) -> Self {
+    pub fn new(data_source: T, query_type: Q, rule_type: R) -> Self {
         Self {
             data_source,
             query_type,
             action: rule_type,
-            _phantom: std::marker::PhantomData,
         }
     }
 }
 
 #[derive(Clone, Debug, Default)]
-pub struct GTxRules<K, Q, R, T>
+pub struct GTxRules<Q, R, T>
 where
-    K: Kind,
     Q: Queryable,
-    R: RuleAction<K, T>,
+    R: RuleAction<T>,
     T: Tier,
 {
-    pub rules: HashMap<K, Vec<GTxRule<K, Q, R, T>>>, // target_kind -> rules
+    pub rules: HashMap<T, Vec<GTxRule<Q, R, T>>>, // target_kind -> rules
 }
 
-impl<K, Q, R, T> GTxRules<K, Q, R, T>
+impl<Q, R, T> GTxRules<Q, R, T>
 where
-    K: Kind,
     Q: Queryable,
-    R: RuleAction<K, T>,
+    R: RuleAction<T>,
     T: Tier,
 {
-    pub fn add_rule(&mut self, rule: GTxRule<K, Q, R, T>) {
+    pub fn add_rule(&mut self, rule: GTxRule<Q, R, T>) {
         self.rules.entry(rule.data_source).or_default().push(rule);
     }
 
-    pub fn get_rule(&self, target: &K, source: &K) -> Option<Vec<&GTxRule<K, Q, R, T>>> {
+    pub fn get_rule(&self, target: &T, source: &T) -> Option<Vec<&GTxRule<Q, R, T>>> {
         self.rules.get(target).map(|rules| {
             rules
                 .iter()
@@ -74,14 +67,13 @@ where
     }
 }
 
-pub trait TxRuleEnforcer<C, K, M, Q, R, T>: Clone + Send + Sync
+pub trait TxRuleEnforcer<C, M, Q, R, T>: Clone + Send + Sync
 where
-    C: PayloadContent<Q>,
-    K: Kind,
-    M: PayloadMetadata,
+    C: PayloadContent,
+    M: PayloadMetadata<Q>,
     Q: Queryable,
-    R: RuleAction<K, T>,
+    R: RuleAction<T>,
     T: Tier,
 {
-    fn enforce_tx_rules(&self, target: &K, payload: GPayload<C, M, Q>) -> GPayload<C, M, Q>;
+    fn enforce_tx_rules(&self, target: &T, payload: GPayload<C, M, Q>) -> GPayload<C, M, Q>;
 }
