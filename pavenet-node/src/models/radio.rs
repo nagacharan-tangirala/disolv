841f0b10b286f3600356117f5a849f76396b1073
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-10-31 17:42:49 +0100

    modified: latency checking, added stat update, reset into the trait

--- pavenet-node/src/models/radio.rs
+++ pavenet-node/src/models/radio.rs
@@ -10,6 +10,7 @@ use pavenet_core::rules::{Actions, Rules};
 use pavenet_engine::hashbrown::HashMap;
 use pavenet_engine::radio::{Channel, Feasibility, IncomingStats};
 use pavenet_engine::rules::TxRuleEnforcer;
+use rand::prelude::SliceRandom;
 use typed_builder::TypedBuilder;
 
 #[derive(Clone, Debug, TypedBuilder)]
@@ -19,9 +20,9 @@ pub struct Radio {
     pub step_size: TimeS,
     pub rng: Pcg64Mcg,
     #[builder(default)]
-    pub in_link_nodes: HashMap<NodeType, Vec<NodeId>>,
+    pub in_link_nodes: HashMap<NodeClass, Vec<NodeId>>,
     #[builder(default)]
-    pub out_link_nodes: HashMap<NodeType, Vec<NodeId>>,
+    pub out_link_nodes: HashMap<NodeClass, Vec<NodeId>>,
     #[builder(default)]
     pub in_stats: InDataStats,
     #[builder(default)]
@@ -30,51 +31,61 @@ pub struct Radio {
     pub transfer_stats: HashMap<NodeId, TransferMetrics>,
 }
 
-impl Channel<NodeContent, PayloadInfo, DataType, Actions, TimeS, Rules> for Radio {
-    fn reset(&mut self) {}
+impl Radio {
+    fn check_feasibility(&mut self, payload: &DPayload) -> bool {
+        let latency_check = match self.latency_model.check_feasibility(&payload.metadata) {
+            Feasibility::Feasible(latency) => {
+                self.in_stats.update_avg_latency(latency);
+                self.transfer_stats
+                    .insert(payload.content.node_info.id, TransferMetrics::new(latency));
+                true
+            }
+            Feasibility::Infeasible(_) => false,
+        };
+        return latency_check;
+    }
+
+    pub(crate) fn transfer_stats(&mut self) -> HashMap<NodeId, TransferMetrics> {
+        self.transfer_stats.clone()
+    }
+}
+
+impl Channel<NodeContent, PayloadInfo, DataType, Actions, NodeClass, Rules> for Radio {
+    fn reset(&mut self) {
+        self.in_stats = InDataStats::default();
+        self.out_stats = OutDataStats::default();
+        self.in_link_nodes.clear();
+        self.out_link_nodes.clear();
+        self.transfer_stats.clear();
+    }
 
     fn can_transfer(&mut self, mut payloads: Vec<DPayload>) -> Vec<DPayload> {
         payloads.shuffle(&mut self.rng);
         let mut valid = Vec::with_capacity(payloads.len());
-        let mut latencies = Vec::with_capacity(payloads.len());
 
         for payload in payloads.into_iter() {
             self.in_stats.add_attempted(&payload.metadata);
+            self.in_link_nodes
+                .entry(payload.content.node_info.node_class)
+                .or_insert(Vec::new())
+                .push(payload.content.node_info.id);
 
-            match self.latency_model.check_feasibility(&payload.metadata) {
-                Feasibility::Feasible(latency) => {
-                    latencies.push(latency);
-                }
-                Feasibility::Infeasible(_) => continue,
+            if !self.check_feasibility(&payload) {
+                continue;
             }
 
-            valid.push(payload);
             self.in_stats.add_feasible(&payload.metadata);
+            valid.push(payload);
         }
-
-        self.in_stats.update_latency(latencies);
         return valid;
     }
 
-    fn apply_tx_rules(&mut self, rules: &Rules, mut payloads: Vec<DPayload>) -> Vec<DPayload> {
+    fn apply_tx_rules(&mut self, rules: &Rules, payloads: Vec<DPayload>) -> Vec<DPayload> {
         let mut to_forward = Vec::with_capacity(payloads.len());
         for payload in payloads.into_iter() {
             let payload = rules.enforce_tx_rules(&self.my_class, payload);
             to_forward.push(payload);
         }
         return to_forward;
-    }
-
-    fn consume(&mut self, payload: &DPayload) {
-        todo!()
-    }
-}
-
-impl Radio {
-    pub fn clear(&mut self) {
-        self.in_stats = InDataStats::default();
-        self.out_stats = OutDataStats::default();
-        self.in_link_nodes.clear();
-        self.out_link_nodes.clear();
     }
 }
