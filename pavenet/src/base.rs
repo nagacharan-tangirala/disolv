34f2487ba87c25023af547edc0dfbcecfeca835a
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-01 13:06:44 +0100

    modified: removed episodes, fixed imports, made some models optional

--- pavenet/src/base.rs
+++ pavenet/src/base.rs
@@ -1,11 +1,14 @@
-use pavenet_core::enums::{DataType, MobilityType, NodeType, TransferMode};
-use pavenet_core::types::TimeStamp;
-use pavenet_models::node::composer::ComposerSettings;
-use pavenet_models::node::responder::ResponderSettings;
-use pavenet_models::node::simplifier::SimplifierSettings;
-use pavenet_models::pool::linker::LinkerSettings;
-use pavenet_models::pool::space::{FieldSettings, SpaceSettings};
-use serde_derive::Deserialize;
+use pavenet_core::bucket::TimeS;
+use pavenet_core::entity::class::NodeClass;
+use pavenet_core::entity::kind::NodeType;
+use pavenet_core::rules::RuleSettings;
+use pavenet_node::models::compose::ComposerSettings;
+use pavenet_node::models::latency::LatencyConfig;
+use pavenet_node::models::linker::LinkerSettings;
+use pavenet_node::models::respond::ResponderSettings;
+use pavenet_node::models::select::SelectorSettings;
+use pavenet_node::models::space::{FieldSettings, MobilitySettings};
+use serde::Deserialize;
 use std::path::PathBuf;
 
 #[serde_with::skip_serializing_none]
@@ -15,16 +18,17 @@ pub struct BaseConfig {
     pub field_settings: FieldSettings,
     pub log_settings: LogSettings,
     pub output_settings: OutputSettings,
-    pub episode_settings: Option<EpisodeSettings>,
-    pub nodes: Vec<NodeSettings>,
+    pub node_settings: Vec<NodeSettings>,
+    pub rule_settings: Vec<RuleSettings>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SimSettings {
     pub sim_name: String,
-    pub sim_duration: u64,
-    pub sim_step: u64,
-    pub sim_streaming_step: u64,
+    pub sim_duration: TimeS,
+    pub sim_step: TimeS,
+    pub sim_streaming_step: TimeS,
+    pub sim_seed: u128,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -45,25 +49,19 @@ pub struct OutputSettings {
 pub struct NodeSettings {
     pub node_type: NodeType,
     pub power_file: String,
-    pub space: SpaceSettings,
+    pub mobility: MobilitySettings,
     pub linker: Option<Vec<LinkerSettings>>,
     pub class: Vec<NodeClassSettings>,
 }
 
-#[serde_with::skip_serializing_none]
 #[derive(Deserialize, Debug, Clone)]
 pub struct NodeClassSettings {
     pub node_share: f32,
-    pub node_class: u32,
-    pub node_order: i32,
+    pub node_class: NodeClass,
+    pub latency: LatencyConfig,
     pub composer: Option<ComposerSettings>,
-    pub simplifier: Option<SimplifierSettings>,
+    pub selector: Option<SelectorSettings>,
     pub responder: Option<ResponderSettings>,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-pub struct EpisodeSettings {
-    pub episode_file: String,
 }
 
 pub struct BaseConfigReader {
