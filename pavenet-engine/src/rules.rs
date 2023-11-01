97d4191c3186b772592ffc8f77bc86a4cdab1b74
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-01 10:40:17 +0100

    simplified: rules implementation

--- pavenet-engine/src/rules.rs
+++ pavenet-engine/src/rules.rs
@@ -1,7 +1,7 @@
 use crate::entity::Tier;
 use crate::payload::{GPayload, PayloadContent, PayloadMetadata};
 use crate::response::Queryable;
-use hashbrown::HashMap;
+use typed_builder::TypedBuilder;
 
 /// A trait that represents the type of a rule.
 pub trait RuleAction<T>: Default + Copy + Clone + Send + Sync
@@ -10,7 +10,7 @@ where
 {
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, TypedBuilder)]
 pub struct GTxRule<Q, R, T>
 where
     Q: Queryable,
@@ -34,36 +34,6 @@ where
             query_type,
             action: rule_type,
         }
-    }
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct GTxRules<Q, R, T>
-where
-    Q: Queryable,
-    R: RuleAction<T>,
-    T: Tier,
-{
-    pub rules: HashMap<T, Vec<GTxRule<Q, R, T>>>, // target_kind -> rules
-}
-
-impl<Q, R, T> GTxRules<Q, R, T>
-where
-    Q: Queryable,
-    R: RuleAction<T>,
-    T: Tier,
-{
-    pub fn add_rule(&mut self, rule: GTxRule<Q, R, T>) {
-        self.rules.entry(rule.data_source).or_default().push(rule);
-    }
-
-    pub fn get_rule(&self, target: &T, source: &T) -> Option<Vec<&GTxRule<Q, R, T>>> {
-        self.rules.get(target).map(|rules| {
-            rules
-                .iter()
-                .filter(|rule| rule.data_source == *source)
-                .collect()
-        })
     }
 }
 
