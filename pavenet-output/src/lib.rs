6a8d35eb38f66ecc10a4eab970324f45ad64ee62
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-29 22:08:24 +0100

    removed: dfs from the output

--- pavenet-output/src/lib.rs
+++ pavenet-output/src/lib.rs
@@ -1,17 +1,4 @@
-pub mod dfs;
+pub mod headers;
 pub mod plots;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod result;
+pub mod writer;
