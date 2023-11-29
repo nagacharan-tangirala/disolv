89c6fdfab9d70c982814de6be4af447a52e56cac
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-29 19:03:29 +0100

    added: node id from core crate to this file

--- pavenet-engine/src/entity.rs
+++ pavenet-engine/src/entity.rs
@@ -1,16 +1,53 @@
-use super::bucket::{Bucket, TimeStamp};
+use super::bucket::Bucket;
+use super::bucket::TimeS;
+use serde::Deserialize;
 use std::fmt::Display;
 use std::hash::Hash;
+use std::str::FromStr;
 
-/// Unique value that identifies an entity. Only a single instance of this type
-/// is allowed. Irrespective of the type of the entity, it should be uniquely identified
-/// by a type implementing this trait.
-/// For example, in a vehicular scenario, both the vehicles and the RSUs can be identified by a
-/// single type (e.g. ID), which implements this trait.
-pub trait Identifier:
-    Default + Display + Clone + Copy + Hash + PartialEq + Eq + Send + Sync + 'static
-{
-    fn as_u32(&self) -> u32;
+#[derive(Deserialize, Default, Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub struct NodeId(u32);
+
+impl Display for NodeId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:09}", self.0)
+    }
+}
+
+impl FromStr for NodeId {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let id = s.parse::<u32>()?;
+        Ok(Self(id))
+    }
+}
+
+impl From<u32> for NodeId {
+    fn from(f: u32) -> Self {
+        Self(f)
+    }
+}
+
+impl From<i64> for NodeId {
+    fn from(f: i64) -> Self {
+        Self(f as u32)
+    }
+}
+
+impl From<i32> for NodeId {
+    fn from(f: i32) -> Self {
+        Self(f as u32)
+    }
+}
+
+impl NodeId {
+    pub fn as_i64(&self) -> i64 {
+        self.0 as i64
+    }
+    pub fn as_u32(&self) -> u32 {
+        self.0
+    }
 }
 
 /// A trait that represents the tier of an entity. Extend this to a custom type that represents
@@ -60,24 +97,20 @@ pub trait MobilityInfo: Copy + Clone {}
 
 /// A trait to get and set the mobility information of an entity. Must extend this for
 /// both the static and mobile entities.
-pub trait Movable<B, M, T>
+pub trait Movable<B, M>
 where
-    B: Bucket<T>,
+    B: Bucket,
     M: MobilityInfo,
-    T: TimeStamp,
 {
     fn mobility(&self) -> &M;
     fn set_mobility(&mut self, bucket: &mut B);
 }
 
 /// A trait that allows an entity to be scheduled for simulation.
-pub trait Schedulable<T>
-where
-    T: TimeStamp,
-{
+pub trait Schedulable {
     fn stop(&mut self);
     fn is_stopped(&self) -> bool;
-    fn time_to_add(&mut self) -> T;
+    fn time_to_add(&mut self) -> TimeS;
 }
 
 /// A trait that represents an entity. Extend this to a custom device type (e.g. struct) that
@@ -93,11 +126,10 @@ where
 /// Starting at this trait will guide you to the other traits that you need to implement for the
 /// device to be simulation-ready.
 /// [tier]: Tier
-pub trait Entity<B, T, Ts>: Schedulable<Ts> + Tiered<T> + Clone + Send + Sync + 'static
+pub trait Entity<B, T>: Schedulable + Tiered<T> + Clone + Send + Sync + 'static
 where
-    B: Bucket<Ts>,
+    B: Bucket,
     T: Tier,
-    Ts: TimeStamp,
 {
     fn uplink_stage(&mut self, bucket: &mut B);
     fn downlink_stage(&mut self, bucket: &mut B);
@@ -105,9 +137,9 @@ where
 
 #[cfg(test)]
 pub(crate) mod tests {
-    use super::{Entity, Identifier, Kind, MobilityInfo, Movable, Schedulable, Tier, Tiered};
-    use crate::bucket::tests::{MyBucket, Ts};
-    use crate::bucket::TimeStamp;
+    use super::{Entity, Kind, MobilityInfo, Movable, NodeId, Schedulable, Tier, Tiered};
+    use crate::bucket::tests::MyBucket;
+    use crate::bucket::TimeS;
     use crate::engine::tests::as_node;
     use krabmaga::engine::schedule::Schedule;
     use std::fmt::{Debug, Display, Formatter};
@@ -132,40 +164,13 @@ pub(crate) mod tests {
         mobility: Mobility,
     }
 
-    impl Movable<MyBucket, Mobility, Ts> for Device {
+    impl Movable<MyBucket, Mobility> for Device {
         fn mobility(&self) -> &Mobility {
             &self.mobility
         }
 
         fn set_mobility(&mut self, my_bucket: &mut MyBucket) {
             todo!("Read from bucket");
-        }
-    }
-
-    #[derive(Default, Clone, Copy, Debug, PartialEq, Eq, Hash)]
-    pub(crate) struct Nid(u32);
-
-    impl Identifier for Nid {
-        fn as_u32(&self) -> u32 {
-            self.0
-        }
-    }
-
-    impl Display for Nid {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{}", self.0)
-        }
-    }
-
-    impl From<i32> for Nid {
-        fn from(value: i32) -> Self {
-            Self(value as u32)
-        }
-    }
-
-    impl Into<u32> for Nid {
-        fn into(self) -> u32 {
-            self.0
         }
     }
 
@@ -210,21 +215,21 @@ pub(crate) mod tests {
 
     #[derive(Default, Clone, Debug)]
     pub(crate) struct TDevice {
-        pub(crate) id: Nid,
+        pub(crate) id: NodeId,
         pub(crate) device_type: DeviceType,
         pub(crate) order: Level,
-        pub(crate) step: Ts,
+        pub(crate) step: TimeS,
     }
 
-    impl Schedulable<Ts> for TDevice {
+    impl Schedulable for TDevice {
         fn stop(&mut self) {}
 
         fn is_stopped(&self) -> bool {
             false
         }
 
-        fn time_to_add(&mut self) -> Ts {
-            Ts::from(0)
+        fn time_to_add(&mut self) -> TimeS {
+            TimeS::from(0)
         }
     }
 
@@ -238,7 +243,7 @@ pub(crate) mod tests {
         }
     }
 
-    impl Entity<MyBucket, Level, Ts> for TDevice {
+    impl Entity<MyBucket, Level> for TDevice {
         fn uplink_stage(&mut self, bucket: &mut MyBucket) {
             self.step = bucket.step;
             println!("step {} in TDevice of type {}", self.step, self.device_type);
@@ -249,32 +254,32 @@ pub(crate) mod tests {
         }
     }
 
-    pub(crate) fn make_device(id: Nid, device_type: DeviceType, order: i32) -> TDevice {
+    pub(crate) fn make_device(id: NodeId, device_type: DeviceType, order: i32) -> TDevice {
         TDevice {
             id,
             device_type,
             order: Level::from(order as u32),
-            step: Ts::default(),
+            step: TimeS::default(),
         }
     }
 
     #[test]
     fn test_device_creation() {
-        let device_a = make_device(Nid::from(1), DeviceType::TypeA, 1);
-        assert_eq!(device_a.id, Nid::from(1));
+        let device_a = make_device(NodeId::from(1), DeviceType::TypeA, 1);
+        assert_eq!(device_a.id, NodeId::from(1));
     }
 
     #[test]
     fn test_ts_addition() {
-        let mut ts = Ts::from(1);
-        ts += Ts::from(1);
-        assert_eq!(ts, Ts::from(2));
+        let mut ts = TimeS::from(1);
+        ts += TimeS::from(1);
+        assert_eq!(ts, TimeS::from(2));
     }
 
     #[test]
     fn test_device_comparison() {
-        let device_a = make_device(Nid::from(1), DeviceType::TypeA, 1);
-        let device_b = make_device(Nid::from(2), DeviceType::TypeB, 2);
+        let device_a = make_device(NodeId::from(1), DeviceType::TypeA, 1);
+        let device_b = make_device(NodeId::from(2), DeviceType::TypeB, 2);
         assert_ne!(device_a.id, device_b.id);
         assert_ne!(device_a.device_type, device_b.device_type);
         assert_eq!(device_a.order, Level::from(1));
@@ -283,21 +288,21 @@ pub(crate) mod tests {
 
     #[test]
     fn test_device_step() {
-        let mut device_a = make_device(Nid::from(1), DeviceType::TypeA, 1);
+        let mut device_a = make_device(NodeId::from(1), DeviceType::TypeA, 1);
         let mut bucket = MyBucket::default();
         device_a.uplink_stage(&mut bucket);
-        assert_eq!(bucket.step, Ts::default());
+        assert_eq!(bucket.step, TimeS::default());
     }
 
     #[test]
     fn test_add_to_schedule() {
         let mut schedule = Schedule::new();
-        let device_a = make_device(Nid::from(1), DeviceType::TypeA, 1);
+        let device_a = make_device(NodeId::from(1), DeviceType::TypeA, 1);
         let node_a = as_node(device_a);
         let x = schedule.schedule_repeating(
             Box::new(node_a.clone()),
-            node_a.node_id.into(),
-            Ts::default().as_f32(),
+            node_a.node_id.as_u32(),
+            TimeS::default().as_f32(),
             0,
         );
         assert_eq!(x, true);
