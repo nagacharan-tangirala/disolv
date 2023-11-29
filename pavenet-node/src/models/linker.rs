08b84462b2e20378dcaa7fd5069ffa4d17fa10c1
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-29 22:09:41 +0100

    fixed: bugs in linker that suppressed some errors

--- pavenet-node/src/models/linker.rs
+++ pavenet-node/src/models/linker.rs
@@ -1,22 +1,16 @@
 use crate::d_model::BucketModel;
-use pavenet_core::bucket::TimeS;
-use pavenet_core::entity::id::NodeId;
+use log::debug;
 use pavenet_core::entity::kind::NodeType;
-use pavenet_core::link::DLinkOptions;
+use pavenet_core::link::DLink; 
+use pavenet_engine::bucket::TimeS;
+use pavenet_engine::entity::NodeId;
 use pavenet_engine::hashbrown::HashMap;
-use pavenet_input::links::data::{LinkMap, LinkReader, LinksFetcher};
+use pavenet_input::links::data::{LinkMap, LinkReader};
 use serde::Deserialize;
 use typed_builder::TypedBuilder;
 
-#[derive(Deserialize, Clone, Debug, Copy)]
-pub enum TransferMode {
-    UDT,
-    BDT,
-}
-
 #[derive(Deserialize, Debug, Clone)]
 pub struct LinkerSettings {
-    pub transfer_mode: TransferMode,
     pub target_type: NodeType,
     pub links_file: String,
     pub range: f32,
@@ -29,11 +23,16 @@ pub struct Linker {
     #[builder(default)]
     pub links: LinkMap,
     #[builder(default)]
-    pub link_cache: HashMap<NodeId, DLinkOptions>,
+    pub link_cache: HashMap<NodeId, Vec<DLink>>,
 }
 
 impl Linker {
-    pub fn links_of(&mut self, node_id: NodeId) -> Option<DLinkOptions> {
+    pub fn links_of(&mut self, node_id: NodeId) -> Option<Vec<DLink>> {
+        debug!(
+            "Linker::links_of node {} are {:?}",
+            node_id,
+            self.link_cache.get(&node_id)
+        );
         self.link_cache.remove(&node_id)
     }
 }
@@ -41,18 +40,16 @@ impl Linker {
 impl BucketModel for Linker {
     fn init(&mut self, step: TimeS) {
         self.links = match self.reader {
-            LinkReader::File(ref mut reader) => reader.fetch_links_data(step).unwrap_or_default(),
-            LinkReader::Stream(ref mut reader) => reader.fetch_links_data(step).unwrap_or_default(),
+            LinkReader::File(ref mut reader) => reader.read_links_data(step),
+            LinkReader::Stream(ref mut reader) => reader.stream_links_data(step),
         };
     }
 
     fn stream_data(&mut self, step: TimeS) {
         match self.reader {
-            LinkReader::Stream(ref mut reader) => {
-                self.links = reader.fetch_links_data(step).unwrap_or_default()
-            }
+            LinkReader::Stream(ref mut reader) => self.links = reader.stream_links_data(step),
             _ => {}
-        }
+        };
     }
 
     fn refresh_cache(&mut self, step: TimeS) {
