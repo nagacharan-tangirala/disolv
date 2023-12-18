cdbbbcdf17dc7976e0c9f4e718df01b6ac6cb0fc
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-12-18 10:33:37 +0100

    fixed: issues with receiving method

--- crates/pavenet-models/src/radio.rs
+++ crates/pavenet-models/src/radio.rs
@@ -200,122 +200,85 @@ impl SlRadio {
                 .to_class(action.to_class)
                 .to_node(action.to_node)
                 .build();
-
-            actions
-                .entry(action.target)
-                .or_insert(HashMap::new())
-                .entry(action.data_type)
-                .or_insert(rule);
+            actions.entry(action.data_type).or_insert(rule);
         }
         self.actions = actions;
     }
 
-    /// Sets the new action for the data blob
-    ///
-    /// # Arguments
-    /// * `data_blob` - The data blob to set the action for
-    /// * `new_action` - The new action to set
-    fn assign_actions(&self, data_blob: &mut DataBlob, new_action: &ActionImpl) {
-        match new_action.action_type {
-            ActionType::Consume => {
-                data_blob.action.action_type = ActionType::Consume;
-            }
-            ActionType::Forward => {
-                if let Some(target_node) = new_action.to_node {
-                    data_blob.action.to_node = Some(target_node);
+    fn measure_sl_rx(&mut self, payloads: &Vec<DPayload>) {
+        let mut rx_order = 1;
+        for payload in payloads.iter() {
+            let mut sl_stats = RxMetrics::new(payload, rx_order);
+            match self
+                .models
+                .latency_type
+                .measure(&sl_stats, &payload.metadata)
+            {
+                Feasibility::Feasible(latency) => sl_stats.latency = latency,
+                Feasibility::Infeasible(latency) => {
+                    sl_stats.latency = latency;
+                    sl_stats.rx_status = RxStatus::Fail;
+                    sl_stats.rx_fail_reason = RxFailReason::LatencyLimit;
+                    self.sl_metrics.push(sl_stats);
+                    rx_order += 1;
+                    continue;
                 }
-                if let Some(target_class) = new_action.to_class {
-                    data_blob.action.to_class = Some(target_class);
-                }
-                if let Some(target_kind) = new_action.to_kind {
-                    data_blob.action.to_kind = Some(target_kind);
-                }
-                data_blob.action.action_type = ActionType::Forward;
-            }
-        };
+            };
+            sl_stats.rx_status = RxStatus::Ok;
+            self.sl_metrics.push(sl_stats);
+            rx_order += 1;
+        }
     }
 
-    /// Checks if the current node should forward the data blob
-    ///
-    /// # Arguments
-    /// * `blob` - The data blob to check
-    /// * `target_info` - The node info of the target node
-    ///
-    /// # Returns
-    /// * `bool` - True if the current node should forward the data blob, false otherwise
-    fn should_i_forward(&self, blob: &DataBlob, target_info: &NodeInfo) -> bool {
-        if blob.action.action_type == ActionType::Consume {
-            error!("This should have been consumed by now");
-            panic!("consume payload appears to be forwarded");
-        }
-        if let Some(target_id) = blob.action.to_node {
-            if target_id == target_info.id {
-                return true;
-            }
-        }
-        if let Some(class) = blob.action.to_class {
-            if class == target_info.node_class {
-                return true;
-            }
-        }
-        if let Some(target_kind) = blob.action.to_kind {
-            if target_info.node_type == target_kind {
-                return true;
-            }
-        }
-        false
+    pub fn sl_metrics(&mut self) -> Vec<RxMetrics> {
+        self.sl_metrics.clone()
     }
 }
 
-impl TxChannel<PayloadInfo, NodeContent> for TxRadio {
-    type C = NodeClass;
-    type D = DataBlob;
-
+impl SlChannel<PayloadInfo, NodeContent> for SlRadio {
     fn reset(&mut self) {
-        self.out_stats.reset();
-        self.out_link_nodes.clear();
+        self.in_stats.reset();
+        self.in_link_nodes.clear();
+        self.sl_metrics.clear();
+        self.sl_payloads.clear();
     }
 
-    fn prepare_blobs_to_fwd(
-        &mut self,
-        target_node_data: &NodeContent,
-        to_forward: &Vec<DPayload>,
-    ) -> Vec<DataBlob> {
-        let mut blobs_to_forward: Vec<DataBlob> = Vec::new();
-        for payload in to_forward.iter() {
-            for blob in payload.metadata.data_blobs.iter() {
-                if self.should_i_forward(blob, &target_node_data.node_info) {
-                    blobs_to_forward.push(blob.to_owned());
-                }
-            }
-        }
-        blobs_to_forward
-    }
-
-    fn prepare_transfer(&mut self, target_class: &NodeClass, mut payload: DPayload) -> DPayload {
+    fn prepare_transfer(&mut self, mut payload: DPayload) -> DPayload {
         self.out_stats.update(&payload.metadata);
-        self.out_link_nodes
-            .entry(payload.node_state.node_info.node_class)
-            .or_default()
-            .push(payload.node_state.node_info.id);
+        self.out_link_nodes.push(payload.node_state.node_info.id);
 
-        let actions = match self.actions.get(target_class) {
-            Some(acts) => acts,
-            None => return payload,
-        };
         payload.metadata.data_blobs.iter_mut().for_each(|blob| {
-            let new_action = match actions.get(&blob.data_type) {
+            let new_action = match self.actions.get(&blob.data_type) {
                 Some(action) => action,
                 None => {
-                    error!(
-                        "No action found for data type {} in class {}",
-                        blob.data_type, target_class
-                    );
+                    error!("No action found for data type {}", blob.data_type);
                     panic!("Action missing for data type {}", blob.data_type);
                 }
             };
-            self.assign_actions(blob, new_action);
+            assign_actions(blob, new_action);
         });
         payload
+    }
+
+    fn do_receive(&mut self, node_info: &NodeContent, mut payloads: Vec<DPayload>) {
+        payloads.shuffle(&mut self.rng);
+        self.measure_sl_rx(&payloads);
+
+        payloads
+            .into_iter()
+            .zip(self.sl_metrics.iter())
+            .for_each(|(payload, rx_stat)| {
+                self.in_stats.add_attempted(&payload.metadata);
+                self.in_link_nodes.push(payload.node_state.node_info.id);
+
+                if rx_stat.rx_status == RxStatus::Ok {
+                    self.in_stats.add_feasible(&payload.metadata);
+                    self.sl_payloads.push(payload.clone());
+                }
+            });
+
+        self.sl_payloads
+            .iter_mut()
+            .for_each(|payload| do_actions(payload, node_info));
     }
 }
