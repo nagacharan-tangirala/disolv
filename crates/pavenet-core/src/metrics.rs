2f000104a92282164cd8185e4267805a18de8ff4
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-01-19 14:41:14 +0100

    added: Bytes as a new type to represent payload size

--- crates/pavenet-core/src/metrics.rs
+++ crates/pavenet-core/src/metrics.rs
@@ -80,8 +80,65 @@ impl Add for Bandwidth {
     }
 }
 
-impl Metric for Bandwidth {
-    fn as_f32(&self) -> f32 {
-        self.0 as f32
+impl SubAssign for Bandwidth {
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0;
     }
 }
+
+impl Metric for Bandwidth {}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, PartialOrd, Default, Copy)]
+pub struct Bytes(u64);
+
+impl Display for Bytes {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}B", self.0)
+    }
+}
+
+impl Bytes {
+    pub fn new(value: u64) -> Self {
+        Self(value)
+    }
+
+    pub fn as_u64(&self) -> u64 {
+        self.0
+    }
+}
+
+impl SubAssign for Bytes {
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0;
+    }
+}
+
+impl AddAssign for Bytes {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+    }
+}
+
+impl Add for Bytes {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self(self.0 + other.0)
+    }
+}
+
+impl Sub for Bytes {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0)
+    }
+}
+
+impl Sum for Bytes {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::default(), |acc, x| acc + x)
+    }
+}
+
+impl Metric for Bytes {}
