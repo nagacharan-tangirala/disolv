c61668d8e8a4b07f3843d72e120297e99a24ac11
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-01-19 14:56:48 +0100

    modified: bandwidth impl to use new metric traits and helpers

--- crates/pavenet-models/src/bandwidth.rs
+++ crates/pavenet-models/src/bandwidth.rs
@@ -1,15 +1,14 @@
 use log::error;
 use pavenet_core::message::PayloadInfo;
 use pavenet_core::metrics::Bandwidth;
-use pavenet_engine::metrics::{Consumable, Feasibility, Measurable, Metric, MetricSettings};
+use pavenet_engine::metrics::{Consumable, Feasibility, Measurable, MetricSettings};
 use serde::Deserialize;
 
 #[serde_with::skip_serializing_none]
 #[derive(Deserialize, Debug, Clone)]
 pub struct BandwidthConfig {
     pub variant: String,
-    pub available: Bandwidth,
-    pub limit: Bandwidth,
+    pub constraint: Bandwidth,
 }
 
 impl MetricSettings for BandwidthConfig {}
@@ -45,9 +44,15 @@ impl Consumable<Bandwidth> for BandwidthType {
         }
     }
 
-    fn get_available(&self) -> Bandwidth {
+    fn constraint(&self) -> Bandwidth {
         match self {
-            BandwidthType::Constant(bandwidth) => bandwidth.get_available(),
+            BandwidthType::Constant(bandwidth) => bandwidth.constraint(),
+        }
+    }
+
+    fn available(&self) -> Bandwidth {
+        match self {
+            BandwidthType::Constant(bandwidth) => bandwidth.available(),
         }
     }
 }
@@ -57,7 +62,7 @@ impl Consumable<Bandwidth> for BandwidthType {
 #[derive(Debug, Clone)]
 pub struct ConstantBandwidth {
     pub available: Bandwidth,
-    pub limit: Bandwidth,
+    pub constraint: Bandwidth,
 }
 
 impl Consumable<Bandwidth> for ConstantBandwidth {
@@ -67,25 +72,29 @@ impl Consumable<Bandwidth> for ConstantBandwidth {
     fn with_settings(settings: Self::S) -> Self {
         Self {
             available: Bandwidth::default(),
-            limit: settings.limit,
+            constraint: settings.constraint,
         }
     }
 
     fn reset(&mut self) {
-        self.available = Bandwidth::default();
+        self.available = self.constraint;
     }
 
     fn consume(&mut self, metadata: &Self::P) -> Feasibility<Bandwidth> {
         let data_bytes = metadata.total_size;
-        if data_bytes > self.available.as_f32() {
+        if data_bytes.as_u64() > self.available.as_u64() {
             Feasibility::Infeasible(Bandwidth::default())
         } else {
-            self.available -= data_bytes;
+            self.available = Bandwidth::new(self.available.as_u64() - data_bytes.as_u64());
             Feasibility::Feasible(self.available)
         }
     }
 
-    fn get_available(&self) -> Bandwidth {
+    fn constraint(&self) -> Bandwidth {
+        self.constraint
+    }
+
+    fn available(&self) -> Bandwidth {
         self.available
     }
 }
