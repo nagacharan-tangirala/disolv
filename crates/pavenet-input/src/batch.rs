118f9cbdaa0bce218d43fb2d17e543d471df78c6
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-02-04 20:45:57 +0100

    added: util methods to convert record batches to rust type vectors

---
+++ crates/pavenet-input/src/batch.rs
@@ -0,0 +1,85 @@
+use crate::columns::TIME_STEP;
+use arrow_array::RecordBatch;
+use log::debug;
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use parquet::file::statistics::Statistics;
+use pavenet_engine::bucket::TimeMS;
+use std::fs::File;
+use std::path::PathBuf;
+
+pub(crate) fn read_f64_column(col_name: &str, record_batch: &RecordBatch) -> Vec<f64> {
+    let array_data = match record_batch.column_by_name(col_name) {
+        Some(column) => column.to_data(),
+        None => panic!("Failed to read column {}", col_name),
+    };
+    let mut column_data = vec![];
+    for i in 0..array_data.buffers().len() {
+        column_data.extend(array_data.buffer::<f64>(i).to_vec());
+    }
+    column_data
+}
+
+pub(crate) fn read_u32_column(col_name: &str, record_batch: &RecordBatch) -> Vec<u32> {
+    let array_data = match record_batch.column_by_name(col_name) {
+        Some(column) => column.to_data(),
+        None => panic!("Failed to read column {}", col_name),
+    };
+    let mut column_data = vec![];
+    for i in 0..array_data.buffers().len() {
+        column_data.extend(array_data.buffer::<u32>(i).to_vec());
+    }
+    column_data
+}
+
+pub(crate) fn read_u64_column(col_name: &str, record_batch: &RecordBatch) -> Vec<u64> {
+    let array_data = match record_batch.column_by_name(col_name) {
+        Some(column) => column.to_data(),
+        None => panic!("Failed to read column {}", col_name),
+    };
+    let mut column_data = vec![];
+    for i in 0..array_data.buffers().len() {
+        column_data.extend(array_data.buffer::<u64>(i).to_vec());
+    }
+    column_data
+}
+
+pub(crate) fn get_row_groups_for_time(
+    file_path: &PathBuf,
+    is_streaming: bool,
+    start_time: TimeMS,
+    end_time: TimeMS,
+) -> Vec<usize> {
+    let par_file = File::open(file_path).unwrap();
+    let reader = SerializedFileReader::new(par_file).unwrap();
+    if !is_streaming {
+        return (0..reader.num_row_groups()).collect();
+    }
+    let mut interested_groups: Vec<usize> = vec![];
+
+    'row_group_loop: for row_group in 0..reader.num_row_groups() {
+        let row_group_reader = match reader.get_row_group(row_group) {
+            Ok(row_group_reader) => row_group_reader,
+            Err(e) => {
+                debug!("Error reading file: {}", e);
+                panic!("Error reading file: {}", e);
+            }
+        };
+
+        for column in row_group_reader.metadata().columns() {
+            if column.column_descr().name() == TIME_STEP {
+                match column.statistics() {
+                    Some(Statistics::Int64(stats)) => {
+                        if start_time.as_i64() >= *stats.max() || end_time.as_i64() <= *stats.min()
+                        {
+                            break 'row_group_loop;
+                        }
+                        interested_groups.push(row_group);
+                    }
+                    _ => panic!("Time step column is not of type int64"),
+                }
+                break;
+            }
+        }
+    }
+    interested_groups
+}
