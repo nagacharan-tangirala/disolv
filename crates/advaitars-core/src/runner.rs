31a6e5454579213ab771bd50eec2d61df8839d02
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-02-26 17:56:31 +0100

    added: ability to quit by pressing q

--- crates/advaitars-core/src/runner.rs
+++ crates/advaitars-core/src/runner.rs
@@ -4,11 +4,12 @@ use crate::scheduler::{DefaultScheduler, Scheduler};
 use advaitars_ui::content::Content;
 use advaitars_ui::handler::{handle_key_events, Message};
 use advaitars_ui::tui::Tui;
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event as CrosstermEvent, Event, KeyCode, KeyEvent, MouseEvent};
+use log::{debug, info};
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
 use std::sync::mpsc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::{io, thread};
 
 pub fn run_simulation<A, B>(scheduler: &mut DefaultScheduler<A, B>)
@@ -16,7 +17,8 @@ where
     A: Agent<B>,
     B: Bucket,
 {
-    let (sender_ui, receiver_ui) = mpsc::sync_channel(2);
+    let (sender_ui, receiver_ui) = mpsc::sync_channel(1);
+    let sender = sender_ui.clone();
     let terminal_event_sender = sender_ui.clone();
     let duration = scheduler.duration.as_u64();
     thread::scope(|s| {
@@ -52,23 +54,49 @@ where
         let end_time = scheduler.duration.as_u64();
         let step_size = scheduler.step_size.as_u64();
         s.spawn(move || {
-            let start_time = 0;
             if step_size == 0 {
                 panic!("Step size cannot be zero");
             }
-            if start_time > end_time {
-                panic!("Start time cannot be greater than end time");
-            }
-            let mut now = start_time;
+            let mut now = 0;
             scheduler.initialize();
             while now < end_time {
                 scheduler.activate();
                 scheduler.collect_stats();
                 now = scheduler.trigger().as_u64();
-                sender_ui.send(Message::CurrentTime(now)).unwrap();
+
+                match terminal_event_sender.send(Message::CurrentTime(now)) {
+                    Ok(_) => {}
+                    Err(err) => {
+                        info!("User must have requested to quit, terminating at {}", now);
+                        scheduler.terminate();
+                        return;
+                    }
+                };
             }
             scheduler.terminate();
             sender_ui.send(Message::Quit).unwrap();
+        });
+
+        s.spawn(move || loop {
+            let tick_rate = Duration::from_millis(1000);
+            let mut message = None;
+            if event::poll(tick_rate).expect("failed to poll new events") {
+                match event::read().expect("unable to read event") {
+                    CrosstermEvent::Key(e) => message = Some(Message::Key(e)),
+                    CrosstermEvent::Mouse(e) => message = Some(Message::Mouse(e)),
+                    CrosstermEvent::Resize(w, h) => message = Some(Message::Resize(w, h)),
+                    CrosstermEvent::FocusGained => {}
+                    CrosstermEvent::FocusLost => {}
+                    CrosstermEvent::Paste(_) => {}
+                };
+
+                if let Some(m) = message {
+                    match sender.send(m) {
+                        Ok(_) => {}
+                        Err(err) => return,
+                    }
+                }
+            }
         });
     });
 }
