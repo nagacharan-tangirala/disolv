e9e8dd00fcee09a9407d98512e0aea54c3d94412
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-02-26 11:35:03 +0100

    moved: engine's radio to core, core's  radio to models

--- crates/advaitars-core/src/radio.rs
+++ crates/advaitars-core/src/radio.rs
@@ -1,136 +1,115 @@
-use crate::entity::{NodeClass, NodeType};
-use crate::message::{DataType, PayloadInfo};
-use crate::metrics::{Bytes, Latency};
-use advaitars_engine::node::NodeId;
-use advaitars_engine::radio::{ActionInfo, Actionable, Actions, GLink, LinkFeatures};
-use serde::Deserialize;
-use std::fmt::Display;
+use crate::agent::AgentClass;
+use crate::agent::AgentId;
+use crate::bucket::Bucket;
+use crate::message::{GPayload, GResponse, Metadata, NodeState, Queryable, Reply, TxReport};
+use std::fmt::Debug;
 use typed_builder::TypedBuilder;
 
-#[derive(Debug, Copy, Clone, Default)]
-pub struct LinkProperties {
-    pub distance: Option<f32>,
-    pub load_factor: Option<f32>,
+/// A trait that contains information about a link. It could be distance, load, etc.
+pub trait LinkFeatures: Copy + Clone + Debug + Default {}
+
+/// A struct that represents a link between two nodes defined by the features F.
+#[derive(Debug, Copy, Clone, Default, TypedBuilder)]
+pub struct GLink<F>
+where
+    F: LinkFeatures,
+{
+    pub target: AgentId,
+    pub properties: F,
 }
 
-impl LinkFeatures for LinkProperties {}
-
-pub type DLink = GLink<LinkProperties>;
-
-#[derive(Deserialize, Clone, Debug, Copy, Eq, PartialEq, Default)]
-pub enum ActionType {
-    #[default]
-    Consume,
-    Forward,
-}
-
-impl Actionable for ActionType {}
-
-#[derive(Clone, Default, Debug, Copy, TypedBuilder)]
-pub struct Action {
-    pub action_type: ActionType,
-    pub to_class: Option<NodeClass>,
-    pub to_node: Option<NodeId>,
-    pub to_kind: Option<NodeType>,
-}
-
-impl ActionInfo for Action {}
-
-pub type DActions = Actions<Action, DataType>;
-
-#[derive(Deserialize, Debug, Clone, Copy)]
-#[serde_with::skip_serializing_none]
-pub struct ActionSettings {
-    pub target: NodeClass,
-    pub data_type: DataType,
-    pub action_type: ActionType,
-    pub to_class: Option<NodeClass>,
-    pub to_node: Option<NodeId>,
-    pub to_kind: Option<NodeType>,
-}
-
-#[derive(Default, Clone, Copy, Debug)]
-pub struct Counts {
-    pub node_count: u32,
-    pub data_size: Bytes,
-    pub data_count: u32,
-}
-
-impl Counts {
-    pub fn reset(&mut self) {
-        self.node_count = 0;
-        self.data_size = Bytes::default();
-        self.data_count = 0;
-    }
-}
-
-impl Display for Counts {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "(node_count: {}, data_size: {}, data_count: {})",
-            self.node_count, self.data_size, self.data_count
-        )
-    }
-}
-
-#[derive(Default, Clone, Copy, Debug)]
-pub struct OutgoingStats {
-    pub attempted: Counts,
-    pub feasible: Counts,
-    pub avg_latency: Latency,
-}
-
-impl Display for OutgoingStats {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "(attempted: {}, feasible: {}, avg_latency: {})",
-            self.attempted, self.feasible, self.avg_latency
-        )
-    }
-}
-
-impl OutgoingStats {
-    pub fn reset(&mut self) {
-        self.attempted.reset();
-        self.feasible.reset();
-        self.avg_latency = Latency::default();
-    }
-
-    pub fn get_success_rate(&self) -> f32 {
-        if self.attempted.node_count == 0 {
-            return 0.0;
+impl<F> GLink<F>
+where
+    F: LinkFeatures,
+{
+    pub fn new(target: AgentId) -> Self {
+        Self {
+            target,
+            properties: F::default(),
         }
-        self.feasible.node_count as f32 / self.attempted.node_count as f32
-    }
-
-    pub fn add_attempted(&mut self, metadata: &PayloadInfo) {
-        self.attempted.node_count += 1;
-        self.attempted.data_size += metadata.total_size;
-        self.attempted.data_count += metadata.total_count;
-    }
-
-    pub fn add_feasible(&mut self, metadata: &PayloadInfo) {
-        self.feasible.node_count += 1;
-        self.feasible.data_size += metadata.total_size;
-        self.feasible.data_count += metadata.total_count;
     }
 }
 
-#[derive(Default, Copy, Clone, Debug)]
-pub struct IncomingStats {
-    pub out_counts: Counts,
+/// Use this trait to mark a type as an action. This can be used to define custom actions
+/// that can be performed on a payload in the form of enum to indicate actions such as
+/// consume, forward, etc.
+pub trait Actionable: Default + Copy + Clone + Send + Sync {}
+
+/// A trait that contains information that can assist in performing an action on a payload.
+/// Use this on a struct that contains information about the action to be performed.
+/// For example, the action can be to forward the payload to a specific node or class.
+pub trait ActionInfo: Copy + Clone + Send + Sync {}
+
+/// A struct that represents an action that can be performed on a payload.
+/// Action can be different for different data types.
+#[derive(Debug, Clone, Default)]
+pub struct Actions<I, Q>
+where
+    I: ActionInfo,
+    Q: Queryable,
+{
+    pub data_type: Vec<Q>,
+    pub action_info: Vec<I>,
 }
 
-impl IncomingStats {
-    pub fn reset(&mut self) {
-        self.out_counts.reset();
+impl<I, Q> Actions<I, Q>
+where
+    I: ActionInfo,
+    Q: Queryable,
+{
+    pub fn add_action(&mut self, data_type: Q, action_info: I) {
+        self.data_type.push(data_type);
+        self.action_info.push(action_info);
     }
 
-    pub fn update(&mut self, metadata: &PayloadInfo) {
-        self.out_counts.node_count += 1;
-        self.out_counts.data_size += metadata.total_size;
-        self.out_counts.data_count += metadata.total_count;
+    pub fn action_for(&self, data_type: &Q) -> Option<&I> {
+        self.data_type
+            .iter()
+            .zip(self.action_info.iter())
+            .find(|(dt, _)| *dt == data_type)
+            .map(|(_, ai)| ai)
     }
+}
+
+/// A trait that an entity must implement to transmit payloads. Transmission of payloads
+/// can be flexibly handled by the entity and can transfer payloads to devices of any tier.
+/// This should be called in the <code>uplink_stage</code> method of the entity.
+pub trait Transmitter<B, F, M, N>
+where
+    B: Bucket,
+    F: LinkFeatures,
+    M: Metadata,
+    N: NodeState,
+{
+    type NodeClass: AgentClass;
+
+    fn transmit(&mut self, payload: GPayload<M, N>, target: GLink<F>, bucket: &mut B);
+    fn transmit_sl(&mut self, payload: GPayload<M, N>, target: GLink<F>, bucket: &mut B);
+}
+
+/// A trait that an entity must implement to receive messages from other entities in the
+/// simulation. The messages can be from the same class or from up/downstream.
+pub trait Receiver<B, M, N>
+where
+    B: Bucket,
+    M: Metadata,
+    N: NodeState,
+{
+    type C: AgentClass;
+
+    fn receive(&mut self, bucket: &mut B) -> Option<Vec<GPayload<M, N>>>;
+    fn receive_sl(&mut self, bucket: &mut B) -> Option<Vec<GPayload<M, N>>>;
+}
+
+/// A trait that an entity must implement to respond to payloads. Transmission of payloads
+/// can be flexibly handled by the entity transfer payloads to devices of any tier.
+/// This should be called in the <code>downlink_stage</code> method of the entity.
+pub trait Responder<B, R, T>
+where
+    B: Bucket,
+    R: Reply,
+    T: TxReport,
+{
+    fn respond(&mut self, response: Option<GResponse<R, T>>, bucket: &mut B);
+    fn respond_sl(&mut self, response: Option<GResponse<R, T>>, bucket: &mut B);
 }
