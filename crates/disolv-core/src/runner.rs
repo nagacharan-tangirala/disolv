bd4b0aacc7f4427a909cdfb889dd546dce7ee7cb
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2024-03-12 13:29:31 +0100

    moved: the key events thread to inside the simulation thread to terminate cleanly

--- crates/disolv-core/src/runner.rs
+++ crates/disolv-core/src/runner.rs
@@ -53,19 +53,34 @@ where
         });
 
         let end_time = scheduler.duration.as_u64();
-        let step_size = scheduler.step_size.as_u64();
         s.spawn(move || {
-            if step_size == 0 {
-                panic!("Step size cannot be zero");
-            }
+            thread::scope(|s2| {
+                s2.spawn(move || {
+                    let tick_rate = Duration::from_millis(500);
+                    let mut message = None;
+                    if event::poll(tick_rate).expect("failed to poll new events") {
+                        match event::read().expect("unable to read event") {
+                            CrosstermEvent::Key(e) => message = Some(Message::Key(e)),
+                            CrosstermEvent::Mouse(e) => message = Some(Message::Mouse(e)),
+                            CrosstermEvent::Resize(w, h) => message = Some(Message::Resize(w, h)),
+                            CrosstermEvent::FocusGained => {}
+                            CrosstermEvent::FocusLost => {}
+                            CrosstermEvent::Paste(_) => {}
+                        };
+
+                        if let Some(m) = message {
+                            sender.send(m).expect("failed to send terminal events");
+                        }
+                    }
+                });
+            });
             let mut now = 0;
             scheduler.initialize();
             while now < end_time {
                 scheduler.activate();
                 scheduler.collect_stats();
                 now = scheduler.trigger().as_u64();
-
-                match terminal_event_sender.send(Message::CurrentTime(now)) {
+                match terminal_sender.send(Message::CurrentTime(now)) {
                     Ok(_) => {}
                     Err(_) => {
                         info!("User must have requested to quit, terminating at {}", now);
@@ -77,45 +92,19 @@ where
             scheduler.terminate();
             sender_ui.send(Message::Quit).unwrap();
         });
-
-        s.spawn(move || loop {
-            let tick_rate = Duration::from_millis(1000);
-            let mut message = None;
-            if event::poll(tick_rate).expect("failed to poll new events") {
-                match event::read().expect("unable to read event") {
-                    CrosstermEvent::Key(e) => message = Some(Message::Key(e)),
-                    CrosstermEvent::Mouse(e) => message = Some(Message::Mouse(e)),
-                    CrosstermEvent::Resize(w, h) => message = Some(Message::Resize(w, h)),
-                    CrosstermEvent::FocusGained => {}
-                    CrosstermEvent::FocusLost => {}
-                    CrosstermEvent::Paste(_) => {}
-                };
-
-                if let Some(m) = message {
-                    match sender.send(m) {
-                        Ok(_) => {}
-                        Err(_) => return,
-                    }
-                }
-            }
-        });
     });
 }
 
 #[cfg(test)]
 pub(crate) mod tests {
     use super::*;
-    use crate::agent::tests::TDevice;
-    use crate::bucket::tests::MyBucket;
     use crate::bucket::TimeMS;
-    use crate::core::tests::create_core;
     use crate::scheduler::tests::create_scheduler;
 
     #[test]
     fn test_run_simulation() {
-        let mut scheduler = create_scheduler();
+        let scheduler = create_scheduler();
         assert_eq!(scheduler.now, TimeMS::from(0));
-        run_simulation(&mut scheduler, SimUIMetadata::default());
-        assert_eq!(scheduler.now, scheduler.duration);
+        run_simulation(scheduler, SimUIMetadata::default());
     }
 }
