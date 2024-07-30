0958c39bce7a53eee2b49658cf0e88cb77b8fd11
charan <nagacharan.tangirala@tum.de>
2024-07-30 21:59:12 +0200

    modified: slice type definition and packet transfer method

--- crates/bin/disolv-v2x/src/models/network.rs
+++ crates/bin/disolv-v2x/src/models/network.rs
@@ -3,15 +3,19 @@ use typed_builder::TypedBuilder;
 
 use disolv_core::bucket::TimeMS;
 use disolv_core::metrics::{Consumable, Feasibility, Measurable};
+use disolv_models::net::network::{NetworkSlice, SliceType};
 
-use crate::net::bandwidth::{BandwidthConfig, BandwidthType};
-use crate::net::latency::{LatencyConfig, LatencyType};
-use crate::net::message::{TxFailReason, TxMetrics, TxStatus, V2XPayload};
+use crate::models::bandwidth::{BandwidthConfig, BandwidthType};
+use crate::models::latency::{LatencyConfig, LatencyType};
+use crate::models::message::{
+    DataBlob, DataType, MessageType, PayloadInfo, TxFailReason, TxMetrics, TxStatus, V2XPayload,
+};
+use crate::v2x::device::DeviceInfo;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SliceSettings {
     pub id: u32,
-    pub name: String,
+    pub name: V2XSlice,
     pub latency: LatencyConfig,
     pub bandwidth: BandwidthConfig,
 }
@@ -26,10 +30,17 @@ pub struct RadioResources {
     pub bandwidth_type: BandwidthType,
 }
 
+#[derive(Deserialize, Default, Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum V2XSlice {
+    #[default]
+    Wireless5G,
+}
+
+impl SliceType for V2XSlice {}
+
 #[derive(Clone, Debug, TypedBuilder)]
 pub struct Slice {
     pub id: u32,
-    pub name: String,
     pub metrics: RadioMetrics,
     pub resources: RadioResources,
     pub step_size: TimeMS,
@@ -37,20 +48,16 @@ pub struct Slice {
     pub tx_order: u32,
 }
 
-impl Slice {
-    pub fn reset(&mut self) {
+impl NetworkSlice<DataType, DataBlob, PayloadInfo, DeviceInfo, MessageType, TxMetrics> for Slice {
+    fn reset(&mut self) {
         self.tx_order = 0;
         self.resources.bandwidth_type.reset();
     }
 
-    pub fn transfer(&mut self, payload: &V2XPayload) -> TxMetrics {
+    fn transfer(&mut self, payload: &V2XPayload) -> TxMetrics {
         self.tx_order += 1;
-        let mut tx_metrics = TxMetrics::new(payload, self.tx_order);
-        match self
-            .metrics
-            .latency_type
-            .measure(&tx_metrics, &payload.metadata)
-        {
+        let mut tx_metrics = TxMetrics::new(&payload, self.tx_order);
+        match self.metrics.latency_type.measure(&payload.metadata) {
             Feasibility::Feasible(latency) => tx_metrics.latency = latency,
             Feasibility::Infeasible(latency) => {
                 tx_metrics.latency = latency;
