be84ba1fe9f6a8455b300c4eae517fef6214771d
charan <nagacharan.tangirala@tum.de>
2024-07-30 21:45:27 +0200

    moved: bytes type definition from models to the core

--- crates/lib/disolv-core/src/metrics.rs
+++ crates/lib/disolv-core/src/metrics.rs
@@ -1,9 +1,71 @@
-use crate::message::{Metadata, TxReport};
+use std::fmt::Display;
+use std::iter::Sum;
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+use serde::{Deserialize, Serialize};
+
+use crate::message::Metadata;
 
 /// A trait that measures some quantity of the radio. It could be a struct or a simple named type.
 /// Any number of metrics can be used to measure the radio usage. The name should be unique and
 /// must be added to the enum that implements the <code>MetricName</code> trait.
 pub trait Metric: Default + PartialEq + PartialOrd + Copy + Clone + Send + Sync {}
+
+/// Each byte is 8 bits, a custom type is defined.
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, PartialOrd, Default, Copy)]
+pub struct Bytes(u64);
+
+impl Display for Bytes {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}B", self.0)
+    }
+}
+
+impl Bytes {
+    pub fn new(value: u64) -> Self {
+        Self(value)
+    }
+
+    pub fn as_u64(&self) -> u64 {
+        self.0
+    }
+}
+
+impl SubAssign for Bytes {
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0;
+    }
+}
+
+impl AddAssign for Bytes {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+    }
+}
+
+impl Add for Bytes {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self(self.0 + other.0)
+    }
+}
+
+impl Sub for Bytes {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0)
+    }
+}
+
+impl Sum for Bytes {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::default(), |acc, x| acc + x)
+    }
+}
+
+impl Metric for Bytes {}
 
 /// An enum that represents the feasibility of a metric. This is used as return type of the
 /// feasibility evaluation so that the caller can get the feasibility and the actual value of the
