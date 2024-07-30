d474d4dcf5a8f8fe2daa811a12ac8773836086fe
charan <nagacharan.tangirala@tum.de>
2024-07-30 21:49:49 +0200

    modified: datalake to be generic and removed the responses part

--- crates/lib/disolv-models/src/bucket/lake.rs
+++ crates/lib/disolv-models/src/bucket/lake.rs
@@ -1,75 +1,52 @@
-use disolv_core::agent::AgentId;
+use disolv_core::agent::{AgentId, AgentProperties};
 use disolv_core::hashbrown::HashMap;
-use disolv_core::message::{AgentState, GPayload, GResponse, Metadata, Reply, TxReport};
+use disolv_core::message::{ContentType, DataUnit, Metadata, Payload, QueryType};
 
 #[derive(Clone, Debug, Default)]
-pub struct DataLake<A, M, R, T>
+pub struct DataLake<C, D, M, P, Q>
 where
-    A: AgentState,
+    C: ContentType,
+    D: DataUnit<C>,
     M: Metadata,
-    R: Reply,
-    T: TxReport,
+    P: AgentProperties,
+    Q: QueryType,
 {
-    pub payloads: HashMap<AgentId, Vec<GPayload<A, M>>>,
-    pub sl_payloads: HashMap<AgentId, Vec<GPayload<A, M>>>,
-    pub responses: HashMap<AgentId, GResponse<R, T>>,
-    pub sl_responses: HashMap<AgentId, GResponse<R, T>>,
+    pub payloads: HashMap<AgentId, Vec<Payload<C, D, M, P, Q>>>,
+    pub sl_payloads: HashMap<AgentId, Vec<Payload<C, D, M, P, Q>>>,
 }
 
-impl<A, M, R, T> DataLake<A, M, R, T>
+impl<C, D, M, P, Q> DataLake<C, D, M, P, Q>
 where
-    A: AgentState,
+    C: ContentType,
+    D: DataUnit<C>,
     M: Metadata,
-    R: Reply,
-    T: TxReport,
+    P: AgentProperties,
+    Q: QueryType,
 {
     pub fn new() -> Self {
         Self {
             payloads: HashMap::new(),
             sl_payloads: HashMap::new(),
-            responses: HashMap::new(),
-            sl_responses: HashMap::new(),
         }
     }
-    pub fn payloads_for(&mut self, agent_id: AgentId) -> Option<Vec<GPayload<A, M>>> {
+    pub fn payloads_for(&mut self, agent_id: AgentId) -> Option<Vec<Payload<C, D, M, P, Q>>> {
         self.payloads.remove(&agent_id)
     }
 
-    pub fn add_payload_to(&mut self, agent_id: AgentId, payload: GPayload<A, M>) {
+    pub fn add_payload_to(&mut self, agent_id: AgentId, payload: Payload<C, D, M, P, Q>) {
         self.payloads.entry(agent_id).or_default().push(payload);
     }
 
-    pub fn add_sl_payload_to(&mut self, agent_id: AgentId, payload: GPayload<A, M>) {
+    pub fn add_sl_payload_to(&mut self, agent_id: AgentId, payload: Payload<C, D, M, P, Q>) {
         self.sl_payloads.entry(agent_id).or_default().push(payload);
     }
 
-    pub fn sl_payloads_for(&mut self, agent_id: AgentId) -> Option<Vec<GPayload<A, M>>> {
+    pub fn sl_payloads_for(&mut self, agent_id: AgentId) -> Option<Vec<Payload<C, D, M, P, Q>>> {
         self.sl_payloads.remove(&agent_id)
-    }
-
-    pub fn response_for(&mut self, agent_id: AgentId) -> Option<GResponse<R, T>> {
-        self.responses.remove(&agent_id)
-    }
-
-    pub fn sl_response_for(&mut self, agent_id: AgentId) -> Option<GResponse<R, T>> {
-        self.sl_responses.remove(&agent_id)
-    }
-
-    pub fn add_sl_response_to(&mut self, agent_id: AgentId, response: GResponse<R, T>) {
-        self.sl_responses.entry(agent_id).or_insert(response);
-    }
-
-    pub fn add_response_to(&mut self, agent_id: AgentId, response: GResponse<R, T>) {
-        self.responses.entry(agent_id).or_insert(response);
     }
 
     pub fn clean_payloads(&mut self) {
         self.payloads.clear();
         self.sl_payloads.clear();
-    }
-
-    pub fn clean_responses(&mut self) {
-        self.responses.clear();
-        self.sl_responses.clear();
     }
 }
