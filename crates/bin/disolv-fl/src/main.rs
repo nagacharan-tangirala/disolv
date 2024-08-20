eebb158d15425ee945cbe363596a6ab7680f9216
charan <nagacharan.tangirala@tum.de>
2024-08-20 13:22:36 +0200

    added: main from the v2x crate, to be adapted

--- crates/bin/disolv-fl/src/main.rs
+++ crates/bin/disolv-fl/src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+use crate::simulation::builder::SimulationBuilder;
+
+mod fl;
+mod models;
+mod simulation;
+
+#[derive(Parser, Debug)]
+#[command(author, version, long_about = None)]
+struct CliArgs {
+    #[arg(short = 'c', long, value_name = "CONFIG_FILE")]
+    config: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+    let start = std::time::Instant::now();
+    let mut builder = SimulationBuilder::new(&args.config);
+    let scheduler = builder.build();
+    run_simulation(scheduler, builder.metadata());
+    let elapsed = start.elapsed();
+    println!("Simulation finished in {} ms.", elapsed.as_millis());
 }
