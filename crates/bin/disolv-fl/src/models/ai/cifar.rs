530689a0313734ed5561b3baf5faaeefca3bd83e
charan <nagacharan.tangirala@tum.de>
2024-08-21 22:31:50 +0200

    added: a placeholder for cifar model

--- crates/bin/disolv-fl/src/models/ai/cifar.rs
+++ crates/bin/disolv-fl/src/models/ai/cifar.rs
@@ -1,9 +1,15 @@
+use burn::nn::conv::Conv2d;
 use burn::prelude::Backend;
-
-use crate::models::ai::mnist::MnistModel;
 
 /// todo: Add cifar10 dataset stuff here.
 #[derive(Clone)]
 pub struct CifarTrainer<B: Backend> {
-    model: MnistModel<B>,
+    pub(crate) device: B::Device,
+    pub(crate) quantity: u64,
+    model: CifarModel<B>,
+}
+
+#[derive(Clone)]
+pub struct CifarModel<B: Backend> {
+    pub conv1: Conv2d<B>,
 }
