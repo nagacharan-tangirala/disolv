f53e3b2b7e79181ccf6623356e38a5c1c35b66b1
charan <nagacharan.tangirala@tum.de>
2024-12-13 22:56:56 +0100

    removed: redundant composer and combined it with the Flcomposer

--- crates/bin/disolv-fl/src/models/ai/compose.rs
+++ crates/bin/disolv-fl/src/models/ai/compose.rs
@@ -1,28 +1,39 @@
+use std::arch::aarch64::float32x2_t;
+
+use parquet::column::writer::ColumnWriter::FloatColumnWriter;
 use serde::Deserialize;
 use typed_builder::TypedBuilder;
 
+use disolv_core::agent::{AgentClass, AgentId};
+use disolv_core::bucket::TimeMS;
 use disolv_core::hashbrown::HashMap;
 use disolv_core::metrics::Bytes;
 use disolv_core::model::{Model, ModelSettings};
 use disolv_core::radio::{Action, Link};
+use disolv_models::device::models::Compose;
 
 use crate::fl::client::AgentInfo;
+use crate::models::ai::data::DataStrategy::Time;
 use crate::models::device::message::{
     FlContent, FlPayload, FlPayloadInfo, Message, MessageType, MessageUnit,
 };
 
-#[derive(Debug, Clone, Deserialize)]
-pub struct FlDataSettings {
+/// Define a struct that contains details about the data sensors that a device can hold.
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub struct V2XDataSource {
+    pub agent_class: AgentClass,
+    pub data_size: Bytes,
+    pub source_step: TimeMS,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+pub struct ComposerSettings {
+    pub name: String,
+    pub source_settings: Vec<V2XDataSource>,
     pub size_map: HashMap<FlContent, Bytes>,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct FlComposerSettings {
-    pub name: String,
-    pub data_settings: FlDataSettings,
-}
-
-impl ModelSettings for FlComposerSettings {}
+impl ModelSettings for ComposerSettings {}
 
 #[derive(Debug, Clone)]
 pub enum FlComposer {
@@ -30,10 +41,12 @@ pub enum FlComposer {
 }
 
 impl FlComposer {
-    pub(crate) fn compose_payload(&mut self, agent_state: &AgentInfo) -> FlPayload {
-        match self {
-            FlComposer::Simple(composer) => composer.compose_payload(agent_state),
-        }
+    pub fn append_units_to(&mut self, payload: &mut FlPayload, units: &mut Vec<MessageUnit>) {
+        units.iter().for_each(|unit| {
+            payload.metadata.total_size += unit.message_size;
+            payload.metadata.total_count += 1;
+        });
+        payload.data_units.append(units);
     }
 
     pub(crate) fn set_message_to_build(&mut self, message: FlMessageToBuild) {
@@ -41,10 +54,21 @@ impl FlComposer {
             FlComposer::Simple(composer) => composer.set_message_to_build(message),
         }
     }
+
+    pub(crate) fn compose(
+        &mut self,
+        at: TimeMS,
+        target_class: &AgentClass,
+        agent_state: &AgentInfo,
+    ) -> FlPayload {
+        match self {
+            FlComposer::Simple(composer) => composer.compose(at, target_class, agent_state),
+        }
+    }
 }
 
 impl Model for FlComposer {
-    type Settings = FlComposerSettings;
+    type Settings = ComposerSettings;
 
     fn with_settings(settings: &Self::Settings) -> Self {
         match settings.name.to_lowercase().as_str() {
@@ -64,57 +88,95 @@ pub struct FlMessageToBuild {
 
 #[derive(Debug, Clone)]
 pub struct SimpleComposer {
-    pub settings: FlDataSettings,
-    pub message_to_send: FlMessageToBuild,
+    pub data_sources: Vec<V2XDataSource>,
+    pub size_map: HashMap<FlContent, Bytes>,
+    pub message_to_send: Option<FlMessageToBuild>,
+    pub step: TimeMS,
 }
 
 impl SimpleComposer {
-    fn new(composer_settings: &FlComposerSettings) -> Self {
+    fn new(composer_settings: &ComposerSettings) -> Self {
         Self {
-            settings: composer_settings.data_settings.to_owned(),
-            message_to_send: FlMessageToBuild::default(),
+            data_sources: composer_settings.source_settings.to_owned(),
+            size_map: composer_settings.size_map.to_owned(),
+            message_to_send: None,
+            step: TimeMS::default(),
         }
     }
 
     fn set_message_to_build(&mut self, message: FlMessageToBuild) {
-        self.message_to_send = message;
+        self.message_to_send = Some(message);
     }
 
-    fn compose_payload(&self, agent_state: &AgentInfo) -> FlPayload {
-        let message_unit = self.compose_data_unit();
-        let payload_info = self.build_metadata(&message_unit);
-        FlPayload::builder()
-            .agent_state(agent_state.clone())
-            .data_units(vec![message_unit])
-            .query_type(Message::FlMessage)
-            .gathered_states(Vec::new())
-            .metadata(payload_info)
-            .build()
+    fn compose_message_units(&self, target_class: &AgentClass) -> Vec<MessageUnit> {
+        let mut message_units = Vec::new();
+        for ds_settings in self.data_sources.iter() {
+            if ds_settings.agent_class != *target_class {
+                continue;
+            }
+            if self.step.as_u64() % ds_settings.source_step.as_u64() != TimeMS::default().as_u64() {
+                continue;
+            }
+        }
+        message_units
     }
 
-    fn compose_data_unit(&self) -> MessageUnit {
+    fn compose_fl_message(&self, message_to_build: FlMessageToBuild) -> MessageUnit {
         let mut message_size = self
-            .settings
             .size_map
-            .get(&self.message_to_send.fl_content)
+            .get(&message_to_build.fl_content)
             .expect("Invalid message")
             .to_owned();
 
-        message_size = Bytes::new(message_size.as_u64() * self.message_to_send.quantity);
+        message_size = Bytes::new(message_size.as_u64() * message_to_build.quantity);
         MessageUnit::builder()
-            .message_type(self.message_to_send.message_type.clone())
+            .message_type(message_to_build.message_type.clone())
             .message_size(message_size)
             .action(Action::default())
-            .fl_content(self.message_to_send.fl_content)
+            .fl_content(message_to_build.fl_content)
             .build()
     }
 
-    fn build_metadata(&self, data_unit: &MessageUnit) -> FlPayloadInfo {
+    fn build_metadata(&self, data_units: &Vec<MessageUnit>) -> FlPayloadInfo {
         let payload_info = FlPayloadInfo::builder()
-            .total_size(data_unit.message_size)
-            .total_count(1)
+            .total_size(data_units.iter().map(|x| x.message_size).sum())
+            .total_count(data_units.len() as u32)
             .selected_link(Link::default())
             .build();
         payload_info
+    }
+
+    fn compose(
+        &mut self,
+        _at: TimeMS,
+        target_class: &AgentClass,
+        agent_state: &AgentInfo,
+    ) -> FlPayload {
+        let mut message_units = Vec::new();
+        if self.data_sources.len() > 0 {
+            message_units = self.compose_message_units(target_class);
+        }
+
+        let mut fl_message = MessageUnit::builder()
+            .message_type(MessageType::SensorData)
+            .message_size(*self.size_map.get(&FlContent::None).unwrap())
+            .action(Action::default())
+            .fl_content(FlContent::None)
+            .build();
+
+        if let Some(message) = self.message_to_send {
+            fl_message = self.compose_fl_message(message);
+            self.message_to_send = None;
+        }
+        message_units.push(fl_message);
+
+        let payload_info = self.build_metadata(&message_units);
+        FlPayload::builder()
+            .metadata(payload_info)
+            .agent_state(agent_state.clone())
+            .gathered_states(Vec::new())
+            .data_units(message_units)
+            .query_type(Message::Sensor)
+            .build()
     }
 }
