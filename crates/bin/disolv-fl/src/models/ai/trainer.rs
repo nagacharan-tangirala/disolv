5085bfd5d76bfa7c2c67a11db93afbbeb5d8038b
charan <nagacharan.tangirala@tum.de>
2024-12-16 18:08:46 +0100

    added: proper saving of the model file to the disk

--- crates/bin/disolv-fl/src/models/ai/trainer.rs
+++ crates/bin/disolv-fl/src/models/ai/trainer.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use burn::backend::wgpu::WgpuDevice;
 use burn::data::dataloader::batcher::Batcher;
 use burn::data::dataset::Dataset;
 use burn::module::Module;
@@ -7,13 +8,17 @@ use burn::prelude::Backend;
 use burn::record::CompactRecorder;
 use burn::tensor::backend::AutodiffBackend;
 use burn::tensor::ElementConversion;
+use log::debug;
 use serde::Deserialize;
 use typed_builder::TypedBuilder;
 
+use disolv_core::bucket::TimeMS;
+
 use crate::models::ai::mnist::{
-    mnist_train, MnistBatcher, MnistModel, MnistTrainConfigSettings, MnistTrainingConfig,
+    mnist_train, MnistBatcher, MnistFlDataset, MnistModel, MnistTrainConfigSettings,
+    MnistTrainingConfig,
 };
-use crate::models::ai::models::{DatasetType, ModelType};
+use crate::models::ai::models::{BatchType, DatasetType, ModelType};
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct TrainerSettings {
@@ -25,7 +30,6 @@ pub struct TrainerSettings {
 #[derive(Clone, TypedBuilder)]
 pub(crate) struct Trainer<B: Backend> {
     pub(crate) model: ModelType<B>,
-    pub(crate) device: B::Device,
     pub(crate) no_of_weights: u64,
     pub(crate) output_path: PathBuf,
     pub(crate) config: MnistTrainingConfig,
@@ -36,7 +40,7 @@ pub(crate) struct Trainer<B: Backend> {
 }
 
 impl<B: AutodiffBackend> Trainer<B> {
-    pub fn train(&mut self) {
+    pub fn train(&mut self, device: &B::Device) {
         self.model = match &self.model {
             ModelType::Mnist(mnist) => {
                 let test_dataset = match &self.test_data {
@@ -53,21 +57,24 @@ impl<B: AutodiffBackend> Trainer<B> {
                     test_dataset.clone(),
                     train_dataset.clone(),
                     mnist.clone(),
-                    self.device.clone(),
+                    device.clone(),
                 )
             }
             ModelType::Cifar(cifar) => unimplemented!("cifar is unimplemented"),
         };
     }
 
-    pub fn save_model_to_file(&self) {
+    pub fn save_model_to_file(&self, step: TimeMS) {
+        let mut model_name = step.to_string();
+        model_name.push_str("_model");
         match &self.model {
             ModelType::Mnist(mnist) => {
                 mnist
                     .clone()
                     .save_file(
                         self.output_path
-                            .join("model")
+                            .join("models")
+                            .join(model_name)
                             .to_str()
                             .expect("invalid output path"),
                         &CompactRecorder::new(),
@@ -78,25 +85,26 @@ impl<B: AutodiffBackend> Trainer<B> {
         }
     }
 
-    pub fn test_model(&self) -> f32 {
+    pub fn test_model(&self, device: &B::Device) -> f32 {
         match &self.model {
-            ModelType::Mnist(mnist) => self.mnist_validation(mnist),
+            ModelType::Mnist(mnist) => self.mnist_validation(mnist, device),
             _ => unimplemented!("other models not supported"),
         }
     }
 
-    fn mnist_validation(&self, mnist_model: &MnistModel<B>) -> f32 {
+    fn mnist_validation(&self, mnist_model: &MnistModel<B>, device: &B::Device) -> f32 {
         let test_dataset = match &self.test_data {
             DatasetType::Mnist(mnist_test) => mnist_test,
-            _ => panic!("Expected mnist test dataset, found something else"),
+            _ => panic!("Expected mnist test dataset"),
         };
 
         let total_tests = (test_dataset.len() as f32 * 0.1) as usize;
         let mut success = 0;
+        debug!("Validating with {} tests", test_dataset.len());
 
         for i in 0..total_tests {
             let item = test_dataset.get(i).expect("failed to get item");
-            let batcher = MnistBatcher::new(self.device.clone());
+            let batcher = MnistBatcher::new(device.clone());
             let batch = batcher.batch(vec![item.clone()]);
 
             let output = mnist_model.forward(batch.images);
