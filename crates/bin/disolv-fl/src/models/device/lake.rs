e1e65cbaaec64242b7176b8587ecf462b311c30a
charan <nagacharan.tangirala@tum.de>
2024-12-16 18:07:33 +0100

    added: time tracking for global model update

--- crates/bin/disolv-fl/src/models/device/lake.rs
+++ crates/bin/disolv-fl/src/models/device/lake.rs
@@ -1,6 +1,8 @@
 use burn::prelude::Backend;
+use log::debug;
 
 use disolv_core::agent::AgentId;
+use disolv_core::bucket::TimeMS;
 use disolv_core::hashbrown::HashMap;
 
 use crate::models::ai::models::ModelType;
@@ -8,6 +10,7 @@ use crate::models::ai::models::ModelType;
 pub(crate) struct ModelLake<B: Backend> {
     pub(crate) global_model: Option<ModelType<B>>,
     model_map: HashMap<AgentId, ModelType<B>>,
+    update_time: TimeMS,
 }
 
 impl<B: Backend> ModelLake<B> {
@@ -15,11 +18,23 @@ impl<B: Backend> ModelLake<B> {
         Self {
             global_model: None,
             model_map: HashMap::new(),
+            update_time: TimeMS::default(),
         }
     }
 
-    pub(crate) fn update_global_model(&mut self, new_model: ModelType<B>) {
+    pub(crate) fn update_time(&self) -> TimeMS {
+        self.update_time
+    }
+
+    pub(crate) fn update_global_model(&mut self, new_model: ModelType<B>, at: TimeMS) {
+        match new_model.clone() {
+            ModelType::Mnist(mnist) => {
+                debug!("new global model with linear1 {:?}", mnist.linear1.weight)
+            }
+            _ => {}
+        }
         self.global_model = Some(new_model);
+        self.update_time = at;
     }
 
     pub(crate) fn add_local_model(&mut self, agent_id: AgentId, model: ModelType<B>) {
