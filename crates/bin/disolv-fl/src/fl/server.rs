6ee156d0126a692506e9d77e81b682bf00922534
charan <nagacharan.tangirala@tum.de>
2024-12-28 17:11:07 +0100

    added: new outputs to the server and handled new incoming message types

--- crates/bin/disolv-fl/src/fl/server.rs
+++ crates/bin/disolv-fl/src/fl/server.rs
@@ -4,9 +4,9 @@ use burn::tensor::backend::AutodiffBackend;
 use log::debug;
 use typed_builder::TypedBuilder;
 
-use disolv_core::agent::{AgentClass, AgentId};
+use disolv_core::agent::AgentClass;
 use disolv_core::bucket::TimeMS;
-use disolv_core::radio::ActionType;
+use disolv_output::tables::model::ModelUpdate;
 
 use crate::fl::bucket::FlBucket;
 use crate::fl::device::DeviceInfo;
@@ -18,7 +18,6 @@ use crate::models::ai::select::ClientSelector;
 use crate::models::ai::times::ServerTimes;
 use crate::models::ai::trainer::Trainer;
 use crate::models::device::message::{FlContent, FlPayload, Message, MessageType};
-use crate::models::device::output::ModelUpdate;
 
 #[derive(Default, Copy, Clone, Debug, Eq, PartialEq)]
 pub(crate) enum ServerState {
@@ -26,6 +25,7 @@ pub(crate) enum ServerState {
     Idle,
     ClientAnalysis,
     ClientSelection,
+    ClientPreparation,
     TrainingRound,
     Aggregation,
 }
@@ -36,6 +36,7 @@ impl Display for ServerState {
             ServerState::Idle => write!(f, "Idle"),
             ServerState::ClientAnalysis => write!(f, "ClientAnalysis"),
             ServerState::ClientSelection => write!(f, "ClientSelection"),
+            ServerState::ClientPreparation => write!(f, "ClientPreparation"),
             ServerState::TrainingRound => write!(f, "TrainingRound"),
             ServerState::Aggregation => write!(f, "Aggregation"),
         }
@@ -92,6 +93,7 @@ impl<B: AutodiffBackend> Server<B> {
                 ServerState::Idle => self.handle_initiation(bucket),
                 ServerState::ClientAnalysis => self.handle_analysis(bucket),
                 ServerState::ClientSelection => self.handle_selection(bucket),
+                ServerState::ClientPreparation => self.handle_preparation(bucket),
                 ServerState::TrainingRound => self.handle_training(bucket),
                 ServerState::Aggregation => self.handle_aggregation(bucket),
             };
@@ -106,6 +108,12 @@ impl<B: AutodiffBackend> Server<B> {
                 ServerState::ClientSelection => {
                     message_draft.message_type = MessageType::KiloByte;
                     message_draft.fl_content = FlContent::ClientSelected;
+                    message_draft.selected_clients =
+                        Some(self.fl_models.client_selector.selected_clients().clone());
+                }
+                ServerState::ClientPreparation => {
+                    message_draft.message_type = MessageType::F64Weights;
+                    message_draft.fl_content = FlContent::GlobalModel;
                     message_draft.selected_clients =
                         Some(self.fl_models.client_selector.selected_clients().clone());
                 }
@@ -127,14 +135,26 @@ impl<B: AutodiffBackend> Server<B> {
                     FlContent::LocalModel => {
                         self.collect_local_model(bucket, &data_unit.device_info)
                     }
-                    FlContent::TrainingFailed => debug!("a client failed to train"),
+                    FlContent::TrainingFailed => debug!("Client failed to train"),
+                    FlContent::ClientPreparing => {
+                        self.acknowledge(&data_unit.device_info, FlContent::ClientPreparing);
+                    }
+                    FlContent::GlobalModelReceived => {
+                        self.acknowledge(&data_unit.device_info, FlContent::GlobalModelReceived);
+                    }
+                    FlContent::Training => {
+                        self.acknowledge(&data_unit.device_info, FlContent::Training);
+                    }
                     _ => panic!("Server should not receive this message"),
                 });
-            // payload
-            //     .data_units
-            //     .iter_mut()
-            //     .for_each(|message| message.action.action_type = ActionType::Consume);
         }
+    }
+
+    fn acknowledge(&self, agent_info: &DeviceInfo, message: FlContent) {
+        debug!(
+            "Client {} acknowledged: {} at step {}",
+            agent_info.id, message, self.step
+        );
     }
 
     fn register_client(&mut self, agent_info: &DeviceInfo) {
@@ -151,7 +171,6 @@ impl<B: AutodiffBackend> Server<B> {
         if self.fl_models.aggregator.is_model_collected(client_info.id) {
             return;
         }
-
         if self.server_state != ServerState::Aggregation {
             return;
         }
@@ -161,15 +180,20 @@ impl<B: AutodiffBackend> Server<B> {
         self.fl_models
             .aggregator
             .add_local_model(local_model, client_info.id);
-        self.write_model_update(
-            bucket,
-            client_info.id,
-            self.server_info.id,
-            ModelLevel::Local,
-            ModelDirection::Received,
-            TrainingStatus::NA,
-            -1.0,
-        );
+
+        if let Some(writer) = &mut bucket.models.results.model {
+            let model_update = ModelUpdate::builder()
+                .time_step(self.step.as_u64())
+                .agent_id(client_info.id.as_u64())
+                .target_id(self.server_info.id.as_u64())
+                .agent_state(self.server_state.to_string())
+                .model(ModelLevel::Local.to_string())
+                .direction(ModelDirection::Received.to_string())
+                .status(TrainingStatus::NA.to_string())
+                .accuracy(-1.0)
+                .build();
+            writer.add_data(model_update);
+        }
     }
 
     fn handle_initiation(&mut self, bucket: &mut FlBucket<B>) -> FlMessageDraft {
@@ -221,11 +245,27 @@ impl<B: AutodiffBackend> Server<B> {
     }
 
     fn handle_selection(&mut self, bucket: &mut FlBucket<B>) -> FlMessageDraft {
-        debug!("Changing from selection to training at {}", self.step);
-        self.server_state = ServerState::TrainingRound;
+        debug!("Changing from selection to preparation at {}", self.step);
+        self.server_state = ServerState::ClientPreparation;
 
         let selected_clients = self.fl_models.client_selector.selected_clients().to_owned();
         debug!("{} is the selected client count", selected_clients.len());
+
+        if let Some(writer) = &mut bucket.models.results.model {
+            selected_clients.iter().for_each(|client_id| {
+                let model_update = ModelUpdate::builder()
+                    .time_step(self.step.as_u64())
+                    .agent_id(self.server_info.id.as_u64())
+                    .target_id(client_id.as_u64())
+                    .agent_state(self.server_state.to_string())
+                    .model(ModelLevel::Global.to_string())
+                    .direction(ModelDirection::Sent.to_string())
+                    .status(TrainingStatus::NA.to_string())
+                    .accuracy(-1.0)
+                    .build();
+                writer.add_data(model_update);
+            })
+        }
 
         FlMessageDraft::builder()
             .message(Message::FlMessage)
@@ -236,11 +276,28 @@ impl<B: AutodiffBackend> Server<B> {
             .build()
     }
 
+    fn handle_preparation(&mut self, bucket: &mut FlBucket<B>) -> FlMessageDraft {
+        debug!("Changing from preparation to training at {}", self.step);
+        self.server_state = ServerState::TrainingRound;
+
+        let selected_clients = self.fl_models.client_selector.selected_clients().to_owned();
+        debug!("{} is the selected client count", selected_clients.len());
+
+        FlMessageDraft::builder()
+            .message(Message::FlMessage)
+            .message_type(MessageType::KiloByte)
+            .quantity(1)
+            .fl_content(FlContent::InitiateTraining)
+            .selected_clients(Some(selected_clients))
+            .build()
+    }
+
     fn handle_training(&mut self, bucket: &mut FlBucket<B>) -> FlMessageDraft {
         debug!("Changing from training to aggregation at {}", self.step);
         self.server_state = ServerState::Aggregation;
 
         let selected_clients = self.fl_models.client_selector.selected_clients().to_owned();
+        debug!("Selected clients are {:?}", selected_clients);
 
         FlMessageDraft::builder()
             .message(Message::FlMessage)
@@ -269,15 +326,20 @@ impl<B: AutodiffBackend> Server<B> {
             .update_global_model(self.fl_models.trainer.model.clone(), self.step);
 
         let model_accuracy = self.fl_models.trainer.test_model(&bucket.models.device);
-        self.write_model_update(
-            bucket,
-            AgentId::from(100000),
-            AgentId::from(100000),
-            ModelLevel::Global,
-            ModelDirection::Received,
-            TrainingStatus::NA,
-            model_accuracy,
-        );
+
+        if let Some(writer) = &mut bucket.models.results.model {
+            let model_update = ModelUpdate::builder()
+                .time_step(self.step.as_u64())
+                .agent_id(self.server_info.id.as_u64())
+                .target_id(self.server_info.id.as_u64())
+                .agent_state(self.server_state.to_string())
+                .model(ModelLevel::Global.to_string())
+                .direction(ModelDirection::NA.to_string())
+                .status(TrainingStatus::NA.to_string())
+                .accuracy(model_accuracy)
+                .build();
+            writer.add_data(model_update);
+        }
 
         let selected_clients = self.fl_models.client_selector.selected_clients().to_owned();
         FlMessageDraft::builder()
@@ -290,38 +352,12 @@ impl<B: AutodiffBackend> Server<B> {
     }
 
     fn write_state_update(&self, bucket: &mut FlBucket<B>) {
-        if let Some(writer) = &mut bucket.models.output.fl_state_writer {
+        if let Some(writer) = &mut bucket.models.results.state {
             writer.add_data(
                 self.step,
                 self.server_info.id,
                 self.server_state.to_string(),
             );
-        }
-    }
-
-    fn write_model_update(
-        &self,
-        bucket: &mut FlBucket<B>,
-        source: AgentId,
-        target: AgentId,
-        level: ModelLevel,
-        direction: ModelDirection,
-        status: TrainingStatus,
-        accuracy: f32,
-    ) {
-        if let Some(writer) = &mut bucket.models.output.fl_model_writer {
-            let model_update = ModelUpdate::builder()
-                .time_step(self.step)
-                .agent_id(source)
-                .target_id(target)
-                .agent_state(self.server_state.to_string())
-                .model(level.to_string())
-                .direction(direction.to_string())
-                .status(status.to_string())
-                .accuracy(accuracy)
-                .build();
-
-            writer.add_data(model_update);
         }
     }
 }
