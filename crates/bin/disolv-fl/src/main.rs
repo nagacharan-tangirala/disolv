bc8cace6222035fe92ebe9fcb8fc0322d347de49
charan <nagacharan.tangirala@tum.de>
2024-12-28 17:14:48 +0100

    added: the new renderer to be sent to the runner

--- crates/bin/disolv-fl/src/main.rs
+++ crates/bin/disolv-fl/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 
+use disolv_runner::runner::run_simulation;
+
 use crate::simulation::builder::SimulationBuilder;
-use crate::simulation::runner::run_simulation;
 
 mod fl;
 mod models;
@@ -18,8 +19,8 @@ fn main() {
     let args = CliArgs::parse();
     let start = std::time::Instant::now();
     let mut builder = SimulationBuilder::new(&args.config);
-    let scheduler = builder.build_with_map();
-    run_simulation(scheduler, builder.metadata());
+    let scheduler = builder.build();
+    run_simulation(scheduler, builder.metadata(), builder.renderer());
     let elapsed = start.elapsed();
     println!("Simulation finished in {} ms.", elapsed.as_millis());
 }
