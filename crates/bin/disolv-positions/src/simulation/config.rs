f7b49895647719aa301c04be98bd28cc40286091
charan <nagacharan.tangirala@tum.de>
2025-01-05 15:58:43 +0100

    modified: the configuration file layout for positions

--- crates/bin/disolv-positions/src/simulation/config.rs
+++ crates/bin/disolv-positions/src/simulation/config.rs
@@ -2,7 +2,6 @@ use std::path::PathBuf;
 
 use serde::Deserialize;
 
-use disolv_core::agent::AgentKind;
 use disolv_core::bucket::TimeMS;
 use disolv_output::logger::LogSettings;
 
@@ -14,24 +13,26 @@ pub struct TimingSettings {
 }
 
 #[derive(Deserialize, Debug, Clone)]
-pub struct PositionFiles {
-    pub network: AgentKind,
-    pub trace: String,
+pub struct TraceSettings {
+    pub input_trace: String,
+    pub input_network: String,
     pub trace_type: String,
+    pub time_conversion: TimeMS,
+    pub output_trace: String,
+    pub starting_id: u64,
 }
 
 #[derive(Deserialize, Debug, Clone)]
-pub struct OutputSettings {
-    pub output_path: String,
-    pub output_file: String,
+pub struct ActivationSettings {
+    pub activation_file: String,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Config {
     pub log_settings: LogSettings,
-    pub output_settings: OutputSettings,
+    pub trace_settings: TraceSettings,
+    pub activation_settings: ActivationSettings,
     pub timing_settings: TimingSettings,
-    pub position_files: PositionFiles,
 }
 
 pub(crate) fn read_config(file_path: &PathBuf) -> Config {
