9797113958e023d065632e8c0e4053138ad449f1
charan <nagacharan.tangirala@tum.de>
2025-01-14 19:32:23 +0100

    removed: client times that is no longer used

--- crates/simulators/disolv-fl/src/models/ai/times.rs
+++ crates/simulators/disolv-fl/src/models/ai/times.rs
@@ -1,11 +1,9 @@
 use serde::Deserialize;
-use typed_builder::TypedBuilder;
 
 use disolv_core::bucket::TimeMS;
 use disolv_core::model::{Model, ModelSettings};
 
 use crate::fl::server::ServerState;
-use crate::models::ai::models::ClientState;
 
 #[derive(Debug, Clone, Copy, Deserialize)]
 pub(crate) struct ServerDurations {
@@ -44,46 +42,6 @@ impl ServerTimes {
                 ServerState::ClientSelection => self.durations.selection,
                 ServerState::TrainingRound => self.durations.round_length,
                 ServerState::Aggregation => self.durations.aggregation,
-            }
-    }
-
-    pub(crate) fn is_time_to_change(&self, now: TimeMS) -> bool {
-        now == self.next_change_at
-    }
-}
-
-#[derive(Debug, Clone, Copy, Deserialize)]
-pub(crate) struct ClientDurations {
-    pub(crate) informing: TimeMS,
-}
-
-impl ModelSettings for ClientDurations {}
-
-#[derive(Debug, Clone, Copy, TypedBuilder)]
-pub(crate) struct ClientTimes {
-    pub(crate) durations: ClientDurations,
-    pub(crate) next_change_at: TimeMS,
-    pub(crate) default_time: TimeMS,
-}
-
-impl Model for ClientTimes {
-    type Settings = ClientDurations;
-
-    fn with_settings(settings: &Self::Settings) -> Self {
-        Self {
-            durations: *settings,
-            next_change_at: TimeMS::default(),
-            default_time: TimeMS::default(),
-        }
-    }
-}
-
-impl ClientTimes {
-    pub(crate) fn update_time(&mut self, now: TimeMS, current_state: ClientState) {
-        self.next_change_at = now
-            + match current_state {
-                ClientState::Informing => self.durations.informing,
-                _ => self.default_time,
             }
     }
 
