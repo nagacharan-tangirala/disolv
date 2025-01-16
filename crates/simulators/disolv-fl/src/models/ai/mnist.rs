4b2f6f36b82775651401945ffa3b5b160940fa71
charan <nagacharan.tangirala@tum.de>
2025-01-16 22:20:55 +0100

    extracted: dataset related stuff into a separate module

--- crates/simulators/disolv-fl/src/models/ai/mnist.rs
+++ crates/simulators/disolv-fl/src/models/ai/mnist.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::path::PathBuf;
 
 use burn::config::Config;
@@ -23,106 +24,12 @@ use typed_builder::TypedBuilder;
 
 use disolv_core::model::{Model, ModelSettings};
 
-use crate::models::ai::models::{BatchType, ModelType};
+use crate::models::data::dataset::SampleBatcher;
+use crate::models::data::mnist::{MnistBatch, MnistFlDataset};
 use crate::simulation::render::CustomRenderer;
 
-#[derive(Default, Clone, Debug)]
-pub struct MnistFlDataset {
-    pub images: Vec<MnistItem>,
-}
-
-impl MnistFlDataset {
-    pub fn new(dataset_type: BatchType) -> Self {
-        let images = match dataset_type {
-            BatchType::Test => MnistDataset::test().iter().collect(),
-            BatchType::Train => MnistDataset::train().iter().collect(),
-        };
-        Self { images }
-    }
-
-    pub fn with_images(images: Vec<MnistItem>) -> Self {
-        Self { images }
-    }
-
-    pub fn split_with_ratios(ratios: Vec<f64>, batch_type: BatchType) -> Vec<MnistFlDataset> {
-        let all_data = MnistFlDataset::new(batch_type).to_owned();
-        let total_samples = all_data.len();
-        let mut data_chunks = Vec::new();
-        let chunk_sizes: Vec<usize> = ratios
-            .iter()
-            .map(|r| (r * total_samples as f64).round() as usize)
-            .collect();
-
-        let mut start = 0;
-        for size in chunk_sizes {
-            if start >= total_samples {
-                break;
-            }
-            let end = usize::min(start + size, total_samples);
-            let chunk = all_data.images[start..end].to_vec();
-            data_chunks.push(MnistFlDataset::with_images(chunk));
-            start = end;
-        }
-        data_chunks
-    }
-}
-
-impl Dataset<MnistItem> for MnistFlDataset {
-    fn get(&self, index: usize) -> Option<MnistItem> {
-        self.images.get(index).cloned()
-    }
-
-    fn len(&self) -> usize {
-        self.images.len()
-    }
-}
-
-#[derive(Clone, TypedBuilder)]
-pub struct MnistBatcher<B: Backend> {
-    pub device: B::Device,
-}
-
-impl<B: Backend> MnistBatcher<B> {
-    pub fn new(device: B::Device) -> Self {
-        Self { device }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct MnistBatch<B: Backend> {
-    pub images: Tensor<B, 3>,
-    pub targets: Tensor<B, 1, Int>,
-}
-
-impl<B: Backend> Batcher<MnistItem, MnistBatch<B>> for MnistBatcher<B> {
-    fn batch(&self, items: Vec<MnistItem>) -> MnistBatch<B> {
-        let images = items
-            .iter()
-            .map(|item| TensorData::from(item.image))
-            .map(|data| Tensor::<B, 2>::from_data(data.convert::<B::FloatElem>(), &self.device))
-            .map(|tensor| tensor.reshape([1, 28, 28]))
-            .map(|tensor| ((tensor / 255) - 0.1307) / 0.3081)
-            .collect();
-
-        let targets = items
-            .iter()
-            .map(|item| {
-                Tensor::<B, 1, Int>::from_data(
-                    TensorData::from([(item.label as i64).elem::<B::IntElem>()]),
-                    &self.device,
-                )
-            })
-            .collect();
-
-        let images = Tensor::cat(images, 0);
-        let targets = Tensor::cat(targets, 0);
-
-        MnistBatch { images, targets }
-    }
-}
-
 #[derive(Clone, Debug, Deserialize)]
-pub struct MnistInputConfigSettings {
+pub struct MnistHyperParameters {
     pub num_epochs: Option<usize>,
     pub batch_size: Option<usize>,
     pub num_workers: Option<usize>,
@@ -133,7 +40,7 @@ pub struct MnistInputConfigSettings {
     pub drop_out: f64,
 }
 
-impl ModelSettings for MnistInputConfigSettings {}
+impl ModelSettings for MnistHyperParameters {}
 
 #[derive(Config, TypedBuilder)]
 pub struct MnistTrainingConfig {
@@ -151,7 +58,7 @@ pub struct MnistTrainingConfig {
 }
 
 impl Model for MnistTrainingConfig {
-    type Settings = MnistInputConfigSettings;
+    type Settings = MnistHyperParameters;
 
     fn with_settings(settings: &Self::Settings) -> Self {
         let optimizer = AdamConfig::new();
@@ -177,9 +84,9 @@ impl Model for MnistTrainingConfig {
 
 #[derive(Module, Debug)]
 pub struct ConvBlock<B: Backend> {
-    conv: Conv2d<B>,
-    norm: BatchNorm<B, 2>,
-    activation: Gelu,
+    pub conv: Conv2d<B>,
+    pub norm: BatchNorm<B, 2>,
+    pub activation: Gelu,
 }
 
 impl<B: Backend> ConvBlock<B> {
