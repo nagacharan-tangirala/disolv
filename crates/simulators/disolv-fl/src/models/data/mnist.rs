abc7f0e6584321a7fe4699c0dfe3bd3c807a02bb
charan <nagacharan.tangirala@tum.de>
2025-01-18 10:48:10 +0100

    added: simple batcher definition for mnist

--- crates/simulators/disolv-fl/src/models/data/mnist.rs
+++ crates/simulators/disolv-fl/src/models/data/mnist.rs
@@ -2,9 +2,9 @@ use burn::data::dataloader::batcher::Batcher;
 use burn::data::dataloader::Dataset;
 use burn::data::dataset::vision::{MnistDataset, MnistItem};
 use burn::prelude::{Backend, ElementConversion, Int, Tensor, TensorData};
+use typed_builder::TypedBuilder;
 
 use crate::models::ai::common::BatchType;
-use crate::models::data::dataset::SampleBatcher;
 
 #[derive(Default, Clone, Debug)]
 pub struct MnistFlDataset {
@@ -63,7 +63,18 @@ pub struct MnistBatch<B: Backend> {
     pub targets: Tensor<B, 1, Int>,
 }
 
-impl<B: Backend> Batcher<MnistItem, MnistBatch<B>> for SampleBatcher<B> {
+#[derive(Clone, TypedBuilder)]
+pub struct MnistBatcher<B: Backend> {
+    pub device: B::Device,
+}
+
+impl<B: Backend> MnistBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+}
+
+impl<B: Backend> Batcher<MnistItem, MnistBatch<B>> for MnistBatcher<B> {
     fn batch(&self, items: Vec<MnistItem>) -> MnistBatch<B> {
         let images = items
             .iter()
